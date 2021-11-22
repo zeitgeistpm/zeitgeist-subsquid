47e30a12434b656e48ecd41273c9eb6038216e0a
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2021-11-23 03:16:21 +0530

    Generate code for CurrencyWithdrawnEvent

--- chain/currency.ts
+++ chain/currency.ts
@@ -90,4 +90,42 @@ export namespace Currency {
       return valid;
     }
   }
+
+  /**
+   *  Withdraw success. \[currency_id, who, amount\]
+   *
+   *  Event parameters: [Currency, AccountId, Balance, ]
+   */
+  export class WithdrawnEvent {
+    public readonly expectedParamTypes = ["CurrencyId", "AccountId", "Balance"];
+
+    constructor(public readonly ctx: SubstrateEvent) {}
+
+    get params(): [CurrencyId, AccountId, Balance] {
+      return [
+        createTypeUnsafe<CurrencyId & Codec>(typeRegistry, "CurrencyId", [
+          this.ctx.params[0].value,
+        ]),
+        createTypeUnsafe<AccountId & Codec>(typeRegistry, "AccountId", [
+          this.ctx.params[1].value,
+        ]),
+        createTypeUnsafe<Balance & Codec>(typeRegistry, "Balance", [
+          this.ctx.params[2].value,
+        ]),
+      ];
+    }
+
+    validateParams(): boolean {
+      if (this.expectedParamTypes.length !== this.ctx.params.length) {
+        return false;
+      }
+      let valid = true;
+      this.expectedParamTypes.forEach((type, i) => {
+        if (type !== this.ctx.params[i].type) {
+          valid = false;
+        }
+      });
+      return valid;
+    }
+  }
 }
