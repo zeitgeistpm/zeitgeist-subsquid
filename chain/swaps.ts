d6640feb7ea030a267c9624a0e226291013bd4df
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-01-19 21:15:59 +0530

    Recreate types for SwapEvent

--- chain/swaps.ts
+++ chain/swaps.ts
@@ -3,125 +3,121 @@ import { SubstrateEvent, SubstrateExtrinsic } from "@subsquid/hydra-common";
 import { Codec } from "@polkadot/types/types";
 import { typeRegistry } from ".";
 
-import {
-  CommonPoolEventParams,
-  Pool,
-  SwapEvent,
-} from "@zeitgeistpm/types/dist/interfaces";
+import { CommonPoolEventParams, Pool, SwapEvent } from '@zeitgeistpm/types/dist/interfaces';
 
 export namespace Swaps {
-  /**
-   *  A new pool has been created. \[CommonPoolEventParams, pool\]
-   *
-   *  Event parameters: [CommonPoolEventParams<<T as frame_system::Config>::AccountId>, Pool<BalanceOf<T>, T::MarketId>, ]
-   */
-  export class PoolCreateEvent {
-    public readonly expectedParamTypes = [
-      "CommonPoolEventParams<<T as frame_system::Config>::AccountId>",
-      "Pool<BalanceOf<T>, T::MarketId>",
-    ];
+    /**
+     *  A new pool has been created. \[CommonPoolEventParams, pool\]
+     *
+     *  Event parameters: [CommonPoolEventParams<<T as frame_system::Config>::AccountId>, Pool<BalanceOf<T>, T::MarketId>, ]
+     */
+    export class PoolCreateEvent {
+        public readonly expectedParamTypes = [
+            'CommonPoolEventParams<<T as frame_system::Config>::AccountId>', 
+            'Pool<BalanceOf<T>, T::MarketId>', 
+        ]
 
-    constructor(public readonly ctx: SubstrateEvent) {}
+        constructor(public readonly ctx: SubstrateEvent) {}
 
-    get params(): [CommonPoolEventParams, Pool] {
-      return [
-        createTypeUnsafe<CommonPoolEventParams & Codec>(
-          typeRegistry,
-          "CommonPoolEventParams",
-          [this.ctx.params[0].value]
-        ),
-        createTypeUnsafe<Pool & Codec>(typeRegistry, "Pool", this.removeWeights([
-          this.ctx.params[1].value,
-        ])),
-      ];
-    }
+        get params(): [CommonPoolEventParams,Pool] {
 
-    validateParams(): boolean {
-      if (this.expectedParamTypes.length !== this.ctx.params.length) {
-        return false;
-      }
-      let valid = true;
-      this.expectedParamTypes.forEach((type, i) => {
-        if (type !== this.ctx.params[i].type) {
-          valid = false;
+            return [
+                createTypeUnsafe<CommonPoolEventParams & Codec>(
+                    typeRegistry,
+                    'CommonPoolEventParams',
+                    [this.ctx.params[0].value]
+                ),
+                createTypeUnsafe<Pool & Codec>(typeRegistry, "Pool", this.removeWeights([
+                    this.ctx.params[1].value,
+                ])),
+            ]
         }
-      });
-      return valid;
-    }
 
-    removeWeights(arg: any[]): any[] {
-      if (arg[0].weights) {
-        arg[0].weights = null;
-      }
-      return arg;
-    }
-  }
-
-  /**
-   *  An exact amount of an asset is entering the pool. \[SwapEvent\]
-   *
-   *  Event parameters: [SwapEvent<<T as frame_system::Config>::AccountId, BalanceOf<T>>, ]
-   */
-  export class SwapExactAmountInEvent {
-    public readonly expectedParamTypes = [
-      "SwapEvent<<T as frame_system::Config>::AccountId, BalanceOf<T>>",
-    ];
-
-    constructor(public readonly ctx: SubstrateEvent) {}
-
-    get params(): [SwapEvent] {
-      return [
-        createTypeUnsafe<SwapEvent & Codec>(typeRegistry, "SwapEvent", [
-          this.ctx.params[0].value,
-        ]),
-      ];
-    }
-
-    validateParams(): boolean {
-      if (this.expectedParamTypes.length !== this.ctx.params.length) {
-        return false;
-      }
-      let valid = true;
-      this.expectedParamTypes.forEach((type, i) => {
-        if (type !== this.ctx.params[i].type) {
-          valid = false;
+        validateParams(): boolean {
+            if (this.expectedParamTypes.length !== this.ctx.params.length) {
+                return false
+            }
+            let valid = true
+            this.expectedParamTypes.forEach((type, i) => {
+                if (type !== this.ctx.params[i].type) {
+                    valid = false
+                }
+            })
+            return valid
         }
-      });
-      return valid;
-    }
-  }
 
-  /**
-   *  An exact amount of an asset is leaving the pool. \[SwapEvent\]
-   *
-   *  Event parameters: [SwapEvent<<T as frame_system::Config>::AccountId, BalanceOf<T>>, ]
-   */
-  export class SwapExactAmountOutEvent {
-    public readonly expectedParamTypes = [
-      "SwapEvent<<T as frame_system::Config>::AccountId, BalanceOf<T>>",
-    ];
-
-    constructor(public readonly ctx: SubstrateEvent) {}
-
-    get params(): [SwapEvent] {
-      return [
-        createTypeUnsafe<SwapEvent & Codec>(typeRegistry, "SwapEvent", [
-          this.ctx.params[0].value,
-        ]),
-      ];
-    }
-
-    validateParams(): boolean {
-      if (this.expectedParamTypes.length !== this.ctx.params.length) {
-        return false;
-      }
-      let valid = true;
-      this.expectedParamTypes.forEach((type, i) => {
-        if (type !== this.ctx.params[i].type) {
-          valid = false;
+        removeWeights(arg: any[]): any[] {
+            if (arg[0].weights) {
+              arg[0].weights = null;
+            }
+            return arg;
         }
-      });
-      return valid;
     }
-  }
+
+    /**
+     *  An exact amount of an asset is entering the pool. \[SwapEvent\]
+     *
+     *  Event parameters: [SwapEvent<<T as frame_system::Config>::AccountId, Asset<T::MarketId>, BalanceOf<T>>, ]
+     */
+    export class SwapExactAmountInEvent {
+        public readonly expectedParamTypes = [
+            'SwapEvent<<T as frame_system::Config>::AccountId, Asset<T::MarketId>, BalanceOf<T>>', 
+        ]
+
+        constructor(public readonly ctx: SubstrateEvent) {}
+
+        get params(): [SwapEvent] {
+
+            return [createTypeUnsafe<SwapEvent & Codec>(
+            typeRegistry, 'SwapEvent', [this.ctx.params[0].value]) ]
+        }
+
+        validateParams(): boolean {
+            if (this.expectedParamTypes.length !== this.ctx.params.length) {
+                return false
+            }
+            let valid = true
+            this.expectedParamTypes.forEach((type, i) => {
+                if (type !== this.ctx.params[i].type) {
+                    valid = false
+                }
+            })
+            return valid
+        }
+
+    }
+
+    /**
+     *  An exact amount of an asset is leaving the pool. \[SwapEvent\]
+     *
+     *  Event parameters: [SwapEvent<<T as frame_system::Config>::AccountId, Asset<T::MarketId>, BalanceOf<T>>, ]
+     */
+    export class SwapExactAmountOutEvent {
+        public readonly expectedParamTypes = [
+            'SwapEvent<<T as frame_system::Config>::AccountId, Asset<T::MarketId>, BalanceOf<T>>', 
+        ]
+
+        constructor(public readonly ctx: SubstrateEvent) {}
+
+        get params(): [SwapEvent] {
+
+            return [createTypeUnsafe<SwapEvent & Codec>(
+            typeRegistry, 'SwapEvent', [this.ctx.params[0].value]) ]
+        }
+
+        validateParams(): boolean {
+            if (this.expectedParamTypes.length !== this.ctx.params.length) {
+                return false
+            }
+            let valid = true
+            this.expectedParamTypes.forEach((type, i) => {
+                if (type !== this.ctx.params[i].type) {
+                    valid = false
+                }
+            })
+            return valid
+        }
+
+    }
+
 }
