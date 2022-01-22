77cfaa32b3d7146c9d7f052bcd12ee156a4a7cf9
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-01-22 11:16:27 +0530

    Generate types for predictionMarkets.MarketResolved

--- chain/prediction-markets.ts
+++ chain/prediction-markets.ts
@@ -552,6 +552,48 @@ export namespace PredictionMarkets {
   }
 
   /**
+   *  A market has been resolved \[market_id, new_market_status, real_outcome\]
+   *
+   *  Event parameters: [MarketIdOf<T>, MarketStatus, OutcomeReport, ]
+   */
+   export class MarketResolvedEvent {
+    public readonly expectedParamTypes = [
+      "MarketIdOf<T>",
+      "MarketStatus",
+      "OutcomeReport",
+    ];
+
+    constructor(public readonly ctx: SubstrateEvent) {}
+
+    get params(): [MarketIdOf, MarketStatus, OutcomeReport] {
+      return [
+        createTypeUnsafe<MarketIdOf & Codec>(typeRegistry, "MarketIdOf", [
+          this.ctx.params[0].value,
+        ]),
+        (createTypeUnsafe<MarketStatus & Codec>(typeRegistry, "MarketStatus", [
+          this.ctx.params[1].value,
+        ]) as any) as MarketStatus,
+        (createTypeUnsafe<OutcomeReport & Codec>(typeRegistry, "OutcomeReport", [
+          this.ctx.params[2].value,
+        ]) as any) as OutcomeReport,
+      ];
+    }
+
+    validateParams(): boolean {
+      if (this.expectedParamTypes.length !== this.ctx.params.length) {
+        return false;
+      }
+      let valid = true;
+      this.expectedParamTypes.forEach((type, i) => {
+        if (type !== this.ctx.params[i].type) {
+          valid = false;
+        }
+      });
+      return valid;
+    }
+  }
+
+  /**
    *  A complete set of shares has been sold \[market_id, seller\]
    *
    *  Event parameters: [MarketIdOf<T>, <T as frame_system::Config>::AccountId, ]
