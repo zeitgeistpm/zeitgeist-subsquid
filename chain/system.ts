3005b7a2a7db2c2a77ebfcc7b8d338c452ac1b73
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2022-01-22 11:13:21 +0530

    Process & map NewAccountEvent (#50)
    * Generate types for NewAccountEvent
    
    * Add event handler systemNewAccount
    
    * Initialise asset balance upon account creation

--- chain/system.ts
+++ chain/system.ts
@@ -3,9 +3,45 @@ import { SubstrateEvent, SubstrateExtrinsic } from "@subsquid/hydra-common";
 import { Codec } from "@polkadot/types/types";
 import { typeRegistry } from ".";
 
-import { DispatchError, DispatchInfo } from "@polkadot/types/interfaces";
+import {
+  AccountId,
+  DispatchError,
+  DispatchInfo,
+} from "@polkadot/types/interfaces";
 
 export namespace System {
+  /**
+   *  A new \[account\] was created.
+   *
+   *  Event parameters: [AccountId, ]
+   */
+  export class NewAccountEvent {
+    public readonly expectedParamTypes = ["AccountId"];
+
+    constructor(public readonly ctx: SubstrateEvent) {}
+
+    get params(): [AccountId] {
+      return [
+        createTypeUnsafe<AccountId & Codec>(typeRegistry, "AccountId", [
+          this.ctx.params[0].value,
+        ]),
+      ];
+    }
+
+    validateParams(): boolean {
+      if (this.expectedParamTypes.length !== this.ctx.params.length) {
+        return false;
+      }
+      let valid = true;
+      this.expectedParamTypes.forEach((type, i) => {
+        if (type !== this.ctx.params[i].type) {
+          valid = false;
+        }
+      });
+      return valid;
+    }
+  }
+
   /**
    *  An extrinsic completed successfully. \[info\]
    *
