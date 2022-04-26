0ab03d8234178b2be9fa5795185e406d1959907a
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-04-26 18:56:45 +0530

    Update processor path

--- scripts/run_processor_local.sh
+++ scripts/run_processor_local.sh
@@ -10,5 +10,5 @@ WS_NODE_URL=ws://127.0.0.1:9944 \
 IPFS_CLIENT_URL=http://127.0.0.1:5001 \
 INDEXER_ENDPOINT_URL=http://127.0.0.1:4010/v1/graphql \
     node -r dotenv-expand/config \
-    lib/processor/index.js \
+    lib/processor/testnet/index.js \
     dotenv_config_debug=true
