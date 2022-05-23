8520bd5338102256afc6d1b252eba0766927b3d2
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-05-23 17:54:03 +0530

    Setup account along with market

--- src/processor/markets.ts
+++ src/processor/markets.ts
@@ -120,7 +120,44 @@ export async function predictionMarketApproved(ctx: EventHandlerContext) {
 
 export async function predictionMarketCreated(ctx: EventHandlerContext) {
     const {store, event, block, extrinsic} = ctx
-    const {marketId, market} = getCreatedEvent(ctx)
+    const {marketId, accountId, market} = getCreatedEvent(ctx)
+
+    if (accountId.length > 2) {
+        const walletId = accountId
+        const acc = await store.get(Account, { where: { accountId: walletId } })
+        if (acc) return
+        const newAcc = new Account()
+        newAcc.id = event.id + '-' + walletId.substring(walletId.length - 5)
+        newAcc.accountId = walletId.toString()
+        newAcc.marketId = +marketId.toString()
+        newAcc.pvalue = 0
+        console.log(`[${event.name}] Saving account: ${JSON.stringify(newAcc, null, 2)}`)
+        await store.save<Account>(newAcc)
+
+        const ab = new AccountBalance()
+        ab.id = event.id + '-' + walletId.substring(walletId.length - 5)
+        ab.account = newAcc
+        ab.assetId = "Ztg"
+        ab.balance = BigInt(0)
+        ab.value = Number(ab.balance)
+        console.log(`[${event.name}] Saving account balance: ${JSON.stringify(ab, null, 2)}`)
+        await store.save<AccountBalance>(ab)
+
+        const hab = new HistoricalAccountBalance()
+        hab.id = event.id + '-' + walletId.substring(walletId.length - 5)
+        hab.accountId = newAcc.accountId
+        hab.event = event.method
+        hab.assetId = ab.assetId
+        hab.dBalance = BigInt(0)
+        hab.balance = ab.balance
+        hab.dValue = Number(hab.dBalance)
+        hab.value = Number(hab.balance)
+        hab.pvalue = 0
+        hab.blockNumber = block.height
+        hab.timestamp = new Date(block.timestamp)
+        console.log(`[${event.name}] Saving historical account balance: ${JSON.stringify(hab, null, 2)}`)
+        await store.save<HistoricalAccountBalance>(hab)
+    }
 
     const marketCreator = encodeAddress(market.creator, +(await Tools.getSDK()).api.consts.system.ss58Prefix.toString())
     const marketOracle = encodeAddress(market.oracle, +(await Tools.getSDK()).api.consts.system.ss58Prefix.toString())
