849a2c138eccbf3d83a721fb07a982b66efd5575
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-05-27 13:16:50 +0530

    Add processor.sh to run for different networks

---
+++ scripts/processor.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$1" = "--start" ]; then
+    echo "Starting processor"
+    yarn db:up && yarn redis:up && yarn db:reset && yarn db:migrate
+elif [ "$1" = "--resume" ]; then
+    echo "Resuming processor"
+else
+    echo "Please specify first argument with --start or --resume"
+    exit
+fi
+
+if [ "$2" = "--local" ]; then
+    yarn build && NODE_ENV=local node lib/processor/index.js
+elif [ "$2" = "--test" ]; then
+    yarn build && NODE_ENV=test node lib/processor/index.js
+elif [ "$2" = "--main" ]; then
+    yarn build && NODE_ENV=main node lib/processor/index.js
+else
+    echo "Please specify second argument with --local or --test or --main"
+fi
