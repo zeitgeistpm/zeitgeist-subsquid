a3b3c8d6674b956604dd24c2352d3d892be55a80
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2022-05-30 09:00:29 +0530

    Set config for processor on battery-station (#135)
    * Rename test to dev config
    
    * Update scripts at package.json
    
    * Add config for t1/t2
    
    * Utilise env config
    
    * Check deps before starting processor

--- scripts/processor.sh
+++ scripts/processor.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# Control points of processor
 if [ "$1" = "--start" ]; then
+    yarn install --frozen-lockfile
     echo "Starting processor"
     yarn db:up && yarn redis:up && yarn db:reset && yarn db:migrate
 elif [ "$1" = "--resume" ]; then
@@ -10,14 +12,19 @@ else
     exit
 fi
 
+# Process data from local-network or battery-station or main-net by passing below argument
 if [ "$2" = "--local" ]; then
     yarn build && NODE_ENV=local node lib/processor/index.js
-elif [ "$2" = "--test" ]; then
-    yarn build && NODE_ENV=test node lib/processor/index.js
+elif [ "$2" = "--dev" ]; then
+    yarn build && NODE_ENV=dev node lib/processor/index.js
+elif [ "$2" = "--t1" ]; then
+    yarn build && NODE_ENV=t1 node lib/processor/index.js
+elif [ "$2" = "--t2" ]; then
+    yarn build && NODE_ENV=t2 node lib/processor/index.js
 elif [ "$2" = "--m1" ]; then
     yarn build && NODE_ENV=m1 node lib/processor/index.js
 elif [ "$2" = "--m2" ]; then
     yarn build && NODE_ENV=m2 node lib/processor/index.js
 else
-    echo "Please specify second argument with --local or --test"
+    echo "Please specify second argument with --local or --dev"
 fi
