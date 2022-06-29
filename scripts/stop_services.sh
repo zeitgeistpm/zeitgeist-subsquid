df0cd1b196c3f42be9b786b0ba5720e7e3713543
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2022-06-29 13:54:54 +0200

    enable execution for script and small update

--- scripts/stop_services.sh
+++ scripts/stop_services.sh
@@ -2,5 +2,5 @@
 
 # to start fresh docker containers for indexer, run this commands
 docker-compose down
-cd indexer && docker-compose down
+cd indexer && docker-compose -f docker-compose.yml -f docker-compose.local.yml down
 docker container prune
