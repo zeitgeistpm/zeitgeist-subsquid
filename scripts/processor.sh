726df684edc64909cf210da56bd33636f6024538
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-08-11 16:18:09 +0530

    Simplify processor script

--- scripts/processor.sh
+++ scripts/processor.sh
@@ -1,36 +1,38 @@
 #!/bin/sh
 
 # Control points of processor
-if [ "$1" = "-start" ]; then
-  echo "Building processor..."
-  docker build . --target processor -t processor
-  echo "Starting processor..."
-  yarn db:up && yarn redis:up && yarn db:reset && yarn db:migrate
-elif [ "$1" = "-resume" ]; then
-  echo "Resuming processor..."
-elif [ "$1" = "-stop" ]; then
+if [ "$1" = "stop" ]; then
   echo "Stopping processor..."
+  docker stop zeitgeist-query-node
   docker stop zeitgeist-processor
   exit
+fi
+
+if [ "$2" = "start" ]; then
+  echo "Building processor..."
+  docker build . --target processor -t processor
+  docker build . --target query-node -t query-node
+  echo "Starting processor..."
+  yarn db:up && yarn redis:up && yarn db:reset && yarn db:migrate
+elif [ "$2" = "resume" ]; then
+  echo "Resuming processor..."
 else
-  echo "Please specify first argument with -start or -resume or -stop"
+  echo "Second argument should be either `start` or `resume`"
   exit
 fi
 
 # Process data from local-network or battery-station or main-net by passing below argument
-# For Linux, replace -p 9090:9090 with --network=host
-if [ "$2" = "-local" ]; then
-  docker run -d -p 9090:9090 --rm -e NODE_ENV=local --env-file=.env.local --name zeitgeist-processor processor
-elif [ "$2" = "-dev" ]; then
-  docker run -d -p 9090:9090 --rm -e NODE_ENV=dev --env-file=.env.dev --name zeitgeist-processor processor
-elif [ "$2" = "-t1" ]; then
-  docker run -d --network=host --rm -e NODE_ENV=t1 --env-file=.env.t1 --name zeitgeist-processor processor
-elif [ "$2" = "-t2" ]; then
-  docker run -d --network=host --rm -e NODE_ENV=t2 --env-file=.env.t2 --name zeitgeist-processor processor
-elif [ "$2" = "-m1" ]; then
-  docker run -d --network=host --rm -e NODE_ENV=m1 --env-file=.env.m1 --name zeitgeist-processor processor
-elif [ "$2" = "-m2" ]; then
-  docker run -d --network=host --rm -e NODE_ENV=m2 --env-file=.env.m2 --name zeitgeist-processor processor
+# For Linux, add --network=host
+if [ "$1" = "dev" ]; then
+  docker run -d -p 9090:9090 --rm -e NODE_ENV=dev --env-file=.env.dev --name=zeitgeist-processor processor
+  docker run -d -p 4350:4350 --rm -e NODE_ENV=dev --env-file=.env.dev --name zeitgeist-query-node query-node
+elif [ "$1" = "local" ]; then
+  sleep 10 # Wait for local node to be up
+  docker run -d --network=host --rm -e NODE_ENV=local --env-file=.env.local --name=zeitgeist-processor processor
+  docker run -d --network=host --rm -e NODE_ENV=local --env-file=.env.local --name=zeitgeist-query-node query-node 
+elif [ "$1" = "t1" ] || [ "$1" = "t2" ] || [ "$1" = "m1" ] || [ "$1" = "m2" ]; then
+  docker run -d --network=host --rm -e NODE_ENV=$1 --env-file=.env.$1 --name=zeitgeist-processor processor
+  docker run -d --network=host --rm -e NODE_ENV=$1 --env-file=.env.$1 --name=zeitgeist-query-node query-node  
 else
-  echo "Please specify second argument with -local or -dev"
+  echo "First argument should be either `local` or `dev`"
 fi
