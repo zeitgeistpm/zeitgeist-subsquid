18402ba97009dd35dd1bedf58cece9d222a0781f
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-08-18 19:24:29 +0530

    Reformat util.ts

--- src/processor/util.ts
+++ src/processor/util.ts
@@ -1,10 +1,10 @@
-import { hexToString, u8aToString } from "@polkadot/util";
+import { hexToString, u8aToString } from '@polkadot/util';
 import SDK from '@zeitgeistpm/sdk'
 import { RedisClient, createClient, ClientOpts } from 'redis'
-import CID from "cids";
-import all from "it-all";
-import { concat, toString } from "uint8arrays";
-import ipfsClient from "ipfs-http-client";
+import CID from 'cids';
+import all from 'it-all';
+import { concat, toString } from 'uint8arrays';
+import ipfsClient from 'ipfs-http-client';
 
 export class Cache {
   private static _instance: Cache
@@ -92,24 +92,24 @@ export class IPFS {
 
   constructor() {
     this.client = ipfsClient({
-      url: process.env["IPFS_CLIENT_URL"] ?? "https://ipfs.zeitgeist.pm",
+      url: process.env.IPFS_CLIENT_URL ?? `https://ipfs.zeitgeist.pm`,
     });
   }
 
-  async add(content: string, hashAlg = "sha3-384"): Promise<CID> {
+  async add(content: string, hashAlg = `sha3-384`): Promise<CID> {
     const { cid } = await this.client.add({ content }, { hashAlg });
     return cid;
   }
 
   async read(partialCid: string): Promise<string> {
     // Old way - backwards compatibility is fun
-    if (partialCid.slice(2, 6) !== "1530") {
+    if (partialCid.slice(2, 6) !== `1530`) {
       const str = hexToString(partialCid);
       return toString(concat(await all(this.client.cat(str))));
     }
 
     // New way
-    const cid = new CID("f0155" + partialCid.slice(2));
+    const cid = new CID(`f0155` + partialCid.slice(2));
     const data = await all(this.client.cat(cid)) as any[];
     return data.map(u8aToString).reduce((p, c) => p + c);
   }
