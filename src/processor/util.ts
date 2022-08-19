cfa404ddb9e1bf227dcdf9475ea30a3625497a9c
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2022-08-19 20:43:33 +0530

    Validate account balances with polkadot.js (#148)
    * Add validation script
    
    * Accept nodeUrl on SDK init
    
    * Reformat util.ts

--- src/processor/util.ts
+++ src/processor/util.ts
@@ -1,10 +1,10 @@
-import { hexToString, u8aToString } from "@polkadot/util";
+import { hexToString, u8aToString } from '@polkadot/util';
 import SDK from '@zeitgeistpm/sdk'
 import { RedisClient, createClient, ClientOpts } from 'redis'
-import CID from "cids";
-import all from "it-all";
-import { concat, toString } from "uint8arrays";
-import ipfsClient from "ipfs-http-client";
+import CID from 'cids';
+import all from 'it-all';
+import { concat, toString } from 'uint8arrays';
+import ipfsClient from 'ipfs-http-client';
 
 export class Cache {
   private static _instance: Cache
@@ -92,24 +92,24 @@ export class IPFS {
 
   constructor() {
     this.client = ipfsClient({
-      url: process.env["IPFS_CLIENT_URL"] ?? "https://ipfs.zeitgeist.pm",
+      url: process.env.IPFS_CLIENT_URL ?? `https://ipfs.zeitgeist.pm`,
     });
   }
 
-  async add(content: string, hashAlg = "sha3-384"): Promise<CID> {
+  async add(content: string, hashAlg = `sha3-384`): Promise<CID> {
     const { cid } = await this.client.add({ content }, { hashAlg });
     return cid;
   }
 
   async read(partialCid: string): Promise<string> {
     // Old way - backwards compatibility is fun
-    if (partialCid.slice(2, 6) !== "1530") {
+    if (partialCid.slice(2, 6) !== `1530`) {
       const str = hexToString(partialCid);
       return toString(concat(await all(this.client.cat(str))));
     }
 
     // New way
-    const cid = new CID("f0155" + partialCid.slice(2));
+    const cid = new CID(`f0155` + partialCid.slice(2));
     const data = await all(this.client.cat(cid)) as any[];
     return data.map(u8aToString).reduce((p, c) => p + c);
   }
@@ -118,22 +118,20 @@ export class IPFS {
 export class Tools {
   private static sdk: SDK
 
-  private static async init(): Promise<SDK> {
-    console.log(`Initializing SDK`)
-    this.sdk = await SDK.initialize(
-      process.env.WS_NODE_URL ?? "wss://bsr.zeitgeist.pm",
-      { ipfsClientUrl: process.env["IPFS_CLIENT_URL"] }
-    );
+  private static async init(nodeUrl: string): Promise<SDK> {
+    console.log(`Initializing SDK...`)
+    this.sdk = await SDK.initialize(nodeUrl, { ipfsClientUrl: process.env.IPFS_CLIENT_URL });
     return this.sdk
   }
 
-  static async getSDK() {
+  static async getSDK(nodeUrl?: string) {
     if (!this.sdk) {
-      return this.init()
+      const url = nodeUrl ?? process.env.WS_NODE_URL ?? `wss://bsr.zeitgeist.pm`;
+      return this.init(url);
     } 
     if (!this.sdk.api.isConnected) {
-      this.sdk.api.connect()
+      this.sdk.api.connect();
     }
-    return this.sdk
+    return this.sdk;
   }
 }
---
+++ test/validateBalances.ts
@@ -0,0 +1,93 @@
+/** 
+ * Script to validate Ztg balance of an account with polkadot.js 
+ */
+import https from 'https'
+import { Tools } from "../src/processor/util"
+import { AccountInfo } from "@polkadot/types/interfaces/system";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // Temporarily stop verifying https certificate
+
+var falseCounter = 0, trueCounter = 0, falseAccs = ``;
+const WS_NODE_URL = `wss://bsr.zeitgeist.pm`;
+const accountLimit = 20; // Number of accounts that need to be validated
+const blockNumber = 1601267; // Balances from polkadot.js will be pulled as on this block number on chain
+
+// GraphQL query for retrieving Ztg balances of accounts
+const query = JSON.stringify({
+  query: `{
+    accountBalances(where: {assetId_eq: "Ztg"}, limit: ${accountLimit}) {
+      account {
+        accountId
+      }
+      assetId
+      balance
+    }
+  }`,
+});
+
+const options = {
+  hostname: 'processor.zeitgeist.pm',
+  path: '/graphql',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': query.length,
+    'User-Agent': 'Node',
+  },
+};
+
+const req = https.request(options, (res) => {
+  let data = '';
+  res.on('data', (d) => {
+    data += d;
+  });
+  res.on('end', async () => {
+    if (res.statusCode == 200) {
+      console.log(`Validating response received from ${options.hostname}`);
+    } else {
+      console.log(JSON.parse(data).errors[0].message);
+      return;
+    }
+    const accounts = JSON.parse(data).data.accountBalances;
+    const sdk = await Tools.getSDK(WS_NODE_URL);
+
+    /**
+     * It is recommended to use `blockNumber` few blocks before finalized head 
+     * of the chain so that subsquid has the data processed in its database.
+     */
+    const blockHash = await sdk.api.rpc.chain.getBlockHash(blockNumber);
+    console.log();
+
+    for (var i = 0; i < accounts.length; i++) {
+      const {data: { free: amt }} = 
+        (await sdk.api.query.system.account.at(blockHash, accounts[i].account.accountId)) as AccountInfo;
+      if (amt.toString() !== accounts[i].balance.toString()) {
+        falseCounter++;
+        falseAccs += accounts[i].account.accountId + `,`;
+        console.log(`\nBalances don't match for ` + accounts[i].account.accountId);
+        console.log(`On Polkadot.js: ` + amt.toBigInt());
+        console.log(`On Subsquid: ` + accounts[i].balance);
+        console.log();
+      } else {
+        trueCounter++;
+        console.log(`Balances match for ` + accounts[i].account.accountId);
+      }
+    }
+    console.log(`\nTotal accounts checked: ${trueCounter+falseCounter}`);
+    console.log(`Balances match for ${trueCounter} accounts`);
+    if (falseCounter > 0) {
+      console.log(`Balances don't match for the below ${falseCounter} account(s):`);
+      var falseAccsList = falseAccs.split(',');
+      for (var a in falseAccsList) {
+        console.log(falseAccsList[a]);
+      }
+    }
+    process.exit();
+  });
+});
+
+req.on('error', (error) => {
+  console.error(error);
+});
+
+req.write(query);
+req.end();
