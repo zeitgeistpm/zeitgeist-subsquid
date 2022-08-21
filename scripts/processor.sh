f92d5da3f9063dd6304f6c082ed66de9a4165dc7
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-08-21 16:29:50 +0530

    Separate query-node from processor.sh

--- scripts/processor.sh
+++ scripts/processor.sh
@@ -3,7 +3,6 @@
 # Control points of processor
 if [ "$1" = "stop" ]; then
   echo "Stopping processor..."
-  docker stop zeitgeist-query-node
   docker stop zeitgeist-processor
   exit
 fi
@@ -11,7 +10,6 @@ fi
 if [ "$2" = "start" ]; then
   echo "Building processor..."
   docker build . --target processor -t processor
-  docker build . --target query-node -t query-node
   echo "Starting processor..."
   yarn db:up && yarn redis:up && yarn db:reset && yarn db:migrate
 elif [ "$2" = "resume" ]; then
@@ -25,14 +23,11 @@ fi
 # For Linux, add --network=host
 if [ "$1" = "dev" ]; then
   docker run -d -p 9090:9090 --rm -e NODE_ENV=dev --env-file=.env.dev --name=zeitgeist-processor processor
-  docker run -d -p 4350:4350 --rm -e NODE_ENV=dev --env-file=.env.dev --name zeitgeist-query-node query-node
 elif [ "$1" = "local" ]; then
   sleep 10 # Wait for local node to be up
   docker run -d --network=host --rm -e NODE_ENV=local --env-file=.env.local --name=zeitgeist-processor processor
-  docker run -d --network=host --rm -e NODE_ENV=local --env-file=.env.local --name=zeitgeist-query-node query-node 
 elif [ "$1" = "t1" ] || [ "$1" = "t2" ] || [ "$1" = "m1" ] || [ "$1" = "m2" ]; then
   docker run -d --network=host --rm -e NODE_ENV=$1 --env-file=.env.$1 --name=zeitgeist-processor processor
-  docker run -d --network=host --rm -e NODE_ENV=$1 --env-file=.env.$1 --name=zeitgeist-query-node query-node  
 else
   echo "First argument should be either `local` or `dev`"
 fi
