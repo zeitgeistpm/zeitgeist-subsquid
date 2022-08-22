1523422e935847074b6ed51683bc2ceef1fc2460
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-08-22 16:48:08 +0530

    Sync with the processor

--- scripts/query-node.sh
+++ scripts/query-node.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
 
-if [ "$2" = "start" ]; then
+__usage="
+Usage: ./scripts/query-node.sh <first> <second>
+
+Options for <first>:
+  local       Query data on local chain
+  dev         Query data on battery-station (dev chain)
+  stop        Stop the already running query-node
+
+Options for <second>:
+  start       Build & start query-node
+"
+
+if [ "$1" = "stop" ]; then
+  echo "Stopping query-node..."
+  docker stop zeitgeist-query-node
+  exit
+elif [ "$2" = "start" ]; then
   echo "Building query-node..."
   docker build . --target query-node -t query-node
   echo "Starting query-node..."
-elif [ "$2" = "stop" ]; then
-  echo "Stopping query-node..."
-  docker stop zeitgeist-query-node
 else
-  echo "Second argument should be either `start` or `resume`"
+  echo "$__usage"
   exit
 fi
 
-# Process data from local-network or battery-station or main-net by passing below argument
-# For Linux, add --network=host
-if [ "$1" = "dev" ]; then
-  docker run -d -p 4350:4350 --rm -e NODE_ENV=dev --env-file=.env.dev --name zeitgeist-query-node query-node
-elif [ "$1" = "local" ] || [ "$1" = "t1" ] || [ "$1" = "t2" ] || [ "$1" = "m1" ] || [ "$1" = "m2" ]; then
+# Add --network=host in case of db connection issues
+if [ "$1" = "local" ] || [ "$1" = "dev" ]; then
+  docker run -d -p 4350:4350 --rm -e NODE_ENV=$1 --env-file=.env.$1 --name zeitgeist-query-node query-node
+elif [ "$1" = "t1" ] || [ "$1" = "t2" ] || [ "$1" = "m1" ] || [ "$1" = "m2" ]; then
   docker run -d --network=host --rm -e NODE_ENV=$1 --env-file=.env.$1 --name=zeitgeist-query-node query-node  
 else
-  echo "First argument should be either `local` or `dev`"
+  echo "$__usage"
 fi
