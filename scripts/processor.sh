050a7162f0ec2b5b637bc2f013cfccf46535489c
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-09-01 16:50:25 +0530

    Enable command for processor reboot

--- scripts/processor.sh
+++ scripts/processor.sh
@@ -11,6 +11,7 @@ Options for <first>:
 Options for <second>:
   start       Build processor services & start processor
   resume      Resume processor
+  restart     Stop, build and start afresh
 "
 
 if [ "$1" = "stop" ]; then
@@ -24,18 +25,27 @@ elif [ "$2" = "start" ]; then
   echo "Starting processor..."
 elif [ "$2" = "resume" ]; then
   echo "Resuming processor..."
+elif [ "$2" = "restart" ]; then
+  echo "Stopping processor..."
+  docker stop zeitgeist-processor
+  echo "Stopping query-node..."
+  docker stop zeitgeist-query-node
+  echo "Building processor..."
+  docker build . --target processor -t processor
+  yarn db:up && yarn redis:up && yarn db:reset && yarn db:migrate
+  echo "Starting processor..."
 else
   echo "$__usage"
   exit
 fi
 
-# Add --network=host in case of db connection issues
+# Use "--network=host" or "-p 4350:4350" in case of db connection issues
 if [ "$1" = "dev" ]; then
   docker run -d -p 9090:9090 --rm -e NODE_ENV=dev --env-file=.env.dev --name=zeitgeist-processor processor
 elif [ "$1" = "local" ]; then
   sleep 15 # Wait for local node to be up
   docker run -d --network=host --rm -e NODE_ENV=local --env-file=.env.local --name=zeitgeist-processor processor
-elif [ "$1" = "t1" ] || [ "$1" = "t2" ] || [ "$1" = "m1" ] || [ "$1" = "m2" ]; then
+elif [ "$1" = "d" ] || [ "$1" = "t1" ] || [ "$1" = "t2" ] || [ "$1" = "m1" ] || [ "$1" = "m2" ]; then
   docker run -d --network=host --rm -e NODE_ENV=$1 --env-file=.env.$1 --name=zeitgeist-processor processor
 else
   echo "$__usage"
