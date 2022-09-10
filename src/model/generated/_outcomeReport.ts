6461b982b8f4f91a75391f837061369763655b32
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-09-11 04:21:13 +0530

    Adapt to changes

--- src/model/generated/_outcomeReport.ts
+++ src/model/generated/_outcomeReport.ts
@@ -6,13 +6,13 @@ import * as marshal from "./marshal"
  */
 export class OutcomeReport {
   private _categorical!: number | undefined | null
-  private _scalar!: number | undefined | null
+  private _scalar!: bigint | undefined | null
 
   constructor(props?: Partial<Omit<OutcomeReport, 'toJSON'>>, json?: any) {
     Object.assign(this, props)
     if (json != null) {
       this._categorical = json.categorical == null ? undefined : marshal.int.fromJSON(json.categorical)
-      this._scalar = json.scalar == null ? undefined : marshal.int.fromJSON(json.scalar)
+      this._scalar = json.scalar == null ? undefined : marshal.bigint.fromJSON(json.scalar)
     }
   }
 
@@ -30,18 +30,18 @@ export class OutcomeReport {
   /**
    * Resultant value from the scalar range. Null if market is categorical
    */
-  get scalar(): number | undefined | null {
+  get scalar(): bigint | undefined | null {
     return this._scalar
   }
 
-  set scalar(value: number | undefined | null) {
+  set scalar(value: bigint | undefined | null) {
     this._scalar = value
   }
 
   toJSON(): object {
     return {
       categorical: this.categorical,
-      scalar: this.scalar,
+      scalar: this.scalar == null ? undefined : marshal.bigint.toJSON(this.scalar),
     }
   }
 }
