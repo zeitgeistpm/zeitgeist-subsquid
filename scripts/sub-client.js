f57a36fcc174b2ea1d2b640fc588dfe7ab82e562
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-10-13 02:04:16 +0530

    Add sample script to subscribe to local query-node

---
+++ scripts/sub-client.js
@@ -0,0 +1,35 @@
+const WebSocket = require('ws');
+const { createClient } = require('graphql-ws');
+
+const port = process.env.GQL_PORT || 4350
+const host = process.env.GQL_HOST || 'localhost'
+const proto = process.env.GQL_PROTO || 'ws'
+
+
+const client = createClient({
+  webSocketImpl: WebSocket,
+  url: `${proto}://${host}:${port}/graphql`,
+});
+
+client.subscribe(
+  {
+    query: `
+    subscription {
+      historicalAccountBalances(limit: 5, orderBy: id_DESC) {
+        accountId
+      }
+    }  
+    `,
+  },
+  {
+    next: (data) => {
+      console.log(`New transfers: ${JSON.stringify(data)}`);
+    },
+    error: (error) => {
+      console.error('error', error);
+    },
+    complete: () => {
+      console.log('done!');
+    },
+  }
+);
