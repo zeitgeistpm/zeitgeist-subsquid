923441ee8b86a44d8c335f4ec13fa37d57f58eb3
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2022-10-12 17:09:17 +0530

    Setup local firesquid (#200)
    * Update outgoing port for indexer prom
    
    * Pass host config from command
    
    * Revisit local env config
    
    * Reframe script for stopping local squid
    
    * Update .env.local
    
    * Make changes for linux
    
    * Remove `host.docker.internal`
    
    * small updates
    
    * Add new local config for mac
    
    Co-authored-by: Tvrtko Majstorovic <tvrtkomaj@gmail.com>

--- scripts/query-node.sh
+++ scripts/query-node.sh
@@ -5,6 +5,7 @@ Usage: ./scripts/query-node.sh <first> <second>
 
 Options for <first>:
   local       Query data on local chain
+  mlocal      Query local chain running on mac
   dev         Query data on battery-station (dev chain)
   stop        Stop the already running query-node
 
@@ -25,9 +26,10 @@ else
   exit
 fi
 
-# Use "--network=host" or "-p 4350:4350" in case of db connection issues
 if [ "$1" = "local" ]; then
   docker run -d --network=host --rm -e NODE_ENV=local --env-file=.env.local --name=zeitgeist-query-node query-node
+elif [ "$1" = "mlocal" ]; then
+  docker run -d -p 4350:4350 --rm -e NODE_ENV=mlocal --env-file=.env.mlocal --name=zeitgeist-query-node query-node
 elif [ "$1" = "dev" ]; then
   docker run -d -p 4350:4350 --rm -e NODE_ENV=dev --env-file=.env.dev --name=zeitgeist-query-node query-node
 elif [ "$1" = "d" ] || [ "$1" = "t1" ] || [ "$1" = "t2" ] || [ "$1" = "m1" ] || [ "$1" = "m2" ]; then
