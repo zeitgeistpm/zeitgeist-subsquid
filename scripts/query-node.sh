e324c023d0332a8a6edfc95753fccddbfd9f43fd
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-10-13 21:28:25 +0530

    Rename docker container name to api

--- scripts/query-node.sh
+++ scripts/query-node.sh
@@ -15,7 +15,7 @@ Options for <second>:
 
 if [ "$1" = "stop" ]; then
   echo "Stopping query-node..."
-  docker stop zeitgeist-query-node
+  docker stop api
   exit
 elif [ "$2" = "start" ]; then
   echo "Building query-node..."
@@ -27,13 +27,13 @@ else
 fi
 
 if [ "$1" = "local" ]; then
-  docker run -d --network=host --rm -e NODE_ENV=local --env-file=.env.local --name=zeitgeist-query-node query-node
+  docker run -d --network=host --rm -e NODE_ENV=local --env-file=.env.local --name=api query-node
 elif [ "$1" = "mlocal" ]; then
-  docker run -d -p 4350:4350 --rm -e NODE_ENV=mlocal --env-file=.env.mlocal --name=zeitgeist-query-node query-node
+  docker run -d -p 4350:4350 --rm -e NODE_ENV=mlocal --env-file=.env.mlocal --name=api query-node
 elif [ "$1" = "dev" ]; then
-  docker run -d -p 4350:4350 --rm -e NODE_ENV=dev --env-file=.env.dev --name=zeitgeist-query-node query-node
+  docker run -d -p 4350:4350 --rm -e NODE_ENV=dev --env-file=.env.dev --name=api query-node
 elif [ "$1" = "d" ] || [ "$1" = "t1" ] || [ "$1" = "t2" ] || [ "$1" = "m1" ] || [ "$1" = "m2" ]; then
-  docker run -d --network=host --rm -e NODE_ENV=$1 --env-file=.env.$1 --name=zeitgeist-query-node query-node  
+  docker run -d --network=host --rm -e NODE_ENV=$1 --env-file=.env.$1 --name=api query-node
 else
   echo "$__usage"
 fi
