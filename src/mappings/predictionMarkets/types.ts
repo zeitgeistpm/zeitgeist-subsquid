ea1f198c3f870cd0122e469f6e6b948ef6d18742
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-10-23 12:33:03 +0530

    Remap specVersion 36 for market creation

--- src/mappings/predictionMarkets/types.ts
+++ src/mappings/predictionMarkets/types.ts
@@ -74,9 +74,16 @@ export function getMarketCreatedEvent(ctx: EventHandlerContext<Store, {event: {a
     market.period.start = market.period.value[0]
     market.period.end = market.period.value[1]
     return { marketId, marketAccountId, market } 
-  } else if (event.isV32 || event.isV36) {
+  } else if (event.isV32) {
     const marketAccountId = ''
     let market = param1 as any
+    market.disputeMechanism = market.mdm
+    market.period.start = market.period.value.start
+    market.period.end = market.period.value.end
+    return { marketId, marketAccountId, market }
+  } else if (event.isV36) {
+    const marketAccountId = encodeAddress(param1, 73)
+    const market = param2 as any
     market.disputeMechanism = market.mdm
     market.period.start = market.period.value.start
     market.period.end = market.period.value.end
