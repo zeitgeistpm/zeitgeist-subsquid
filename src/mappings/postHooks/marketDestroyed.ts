76b483ee8f6691bf97ea5cadb630723920154ec5
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-11-10 19:06:12 +0530

    Resolve db txn conflicts

--- src/mappings/postHooks/marketDestroyed.ts
+++ src/mappings/postHooks/marketDestroyed.ts
@@ -9,22 +9,24 @@ export async function destroyMarkets(ctx: BlockHandlerContext<Store>) {
     191, 202, 232, 239, 252, 253, 257, 258, 265, 266, 283, 284, 312, 328, 333, 334, 335, 344,
   ];
 
-  mIds.forEach(async (mId) => {
-    let market = await ctx.store.get(Market, { where: { marketId: mId } });
-    if (!market) return;
-
-    market.status = 'Destroyed';
-    console.log(`[postHooks.destroyMarkets] Saving market: ${JSON.stringify(market, null, 2)}`);
-    await ctx.store.save<Market>(market);
-
-    let hm = new HistoricalMarket();
-    hm.id = '0000579140-000000-9634e-' + market.marketId;
-    hm.marketId = market.marketId;
-    hm.event = 'MarketDestroyed';
-    hm.status = market.status;
-    hm.blockNumber = ctx.block.height;
-    hm.timestamp = new Date(ctx.block.timestamp);
-    console.log(`[postHooks.destroyMarkets] Saving historical market: ${JSON.stringify(hm, null, 2)}`);
-    await ctx.store.save<HistoricalMarket>(hm);
-  });
+  await Promise.all(
+    mIds.map(async (mId) => {
+      let market = await ctx.store.get(Market, { where: { marketId: mId } });
+      if (!market) return;
+  
+      market.status = 'Destroyed';
+      console.log(`[postHooks.destroyMarkets] Saving market: ${JSON.stringify(market, null, 2)}`);
+      await ctx.store.save<Market>(market);
+  
+      let hm = new HistoricalMarket();
+      hm.id = '0000579140-000000-9634e-' + market.marketId;
+      hm.marketId = market.marketId;
+      hm.event = 'MarketDestroyed';
+      hm.status = market.status;
+      hm.blockNumber = ctx.block.height;
+      hm.timestamp = new Date(ctx.block.timestamp);
+      console.log(`[postHooks.destroyMarkets] Saving historical market: ${JSON.stringify(hm, null, 2)}`);
+      await ctx.store.save<HistoricalMarket>(hm);
+    })
+  )
 }
