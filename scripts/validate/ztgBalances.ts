8b91c274d219edf05173d094c7fa9cc6a93cdfd4
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-01-08 16:52:23 +0530

    Refactor ztg balance validation script

--- scripts/validate/ztgBalances.ts
+++ scripts/validate/ztgBalances.ts
@@ -1,16 +1,14 @@
-/** 
+/**
  * Script to validate Ztg balance of an account against on-chain balance
- * Run using `ts-node scripts/validate/accountBalances.ts wss://bsr.zeitgeist.pm processor.zeitgeist.pm`
+ * Run using `ts-node scripts/validate/ztgBalances.ts wss://bsr.zeitgeist.pm processor.bsr.zeitgeist.pm`
  */
 import { AccountInfo } from '@polkadot/types/interfaces/system';
 import https from 'https';
 import { Tools } from '../../src/mappings/util';
 
-// Modify values as per requirement
 const WS_NODE_URL = process.argv[2];
 const QUERY_NODE_HOSTNAME = process.argv[3];
 
-// GraphQL query for retrieving Ztg balances of accounts
 const query = JSON.stringify({
   query: `{
     accountBalances(where: {assetId_eq: "Ztg"}) {
@@ -48,39 +46,51 @@ const req = https.request(options, (res) => {
       console.log(JSON.parse(data).errors[0].message);
       return;
     }
-    let falseCounter = 0, trueCounter = 0, falseAccs = ``;
     const accounts = JSON.parse(data).data.accountBalances;
+    console.log(`Number of accounts fetched : ${accounts.length}`);
+
     const sdk = await Tools.getSDK(WS_NODE_URL);
+    const blockHash = await sdk.api.rpc.chain.getBlockHash(
+      JSON.parse(data).data.squidStatus.height
+    );
 
-    /**
-     * It is recommended to use `BLOCK_NUMBER` few blocks before finalized head 
-     * of the chain so that subsquid has the data processed in its database.
-     */
-    const blockHash = await sdk.api.rpc.chain.getBlockHash(JSON.parse(data).data.squidStatus.height);
-    console.log();
-
+    let falseCounter = 0,
+      trueCounter = 0,
+      falseAccs = ``;
     for (let i = 0; i < accounts.length; i++) {
-      const {data: { free: amt }} = 
-        (await sdk.api.query.system.account.at(blockHash, accounts[i].account.accountId)) as AccountInfo;
+      const {
+        data: { free: amt },
+      } = (await sdk.api.query.system.account.at(
+        blockHash,
+        accounts[i].account.accountId
+      )) as AccountInfo;
       if (amt.toString() !== accounts[i].balance.toString()) {
         falseCounter++;
         falseAccs += accounts[i].account.accountId + `,`;
-        console.log(`\n${trueCounter+falseCounter}. Balances don't match for ` + accounts[i].account.accountId);
+        console.log(
+          `\n${trueCounter + falseCounter}. Balances don't match for ` +
+            accounts[i].account.accountId
+        );
         console.log(`On Chain: ` + amt.toBigInt());
         console.log(`On Subsquid: ` + accounts[i].balance);
         console.log();
       } else {
         trueCounter++;
-        console.log(`${trueCounter+falseCounter}. Balances match for ` + accounts[i].account.accountId);
+        console.log(
+          `${trueCounter + falseCounter}. Balances match for ` +
+            accounts[i].account.accountId
+        );
       }
     }
     sdk.api.disconnect();
 
-    console.log(`\nTotal accounts checked: ${trueCounter+falseCounter}`);
+    console.log(`\nTotal accounts checked: ${trueCounter + falseCounter}`);
     console.log(`Balances match for ${trueCounter} accounts`);
     if (falseCounter > 0) {
       const falseAccsList = falseAccs.split(',');
-      console.log(`Balances don't match for the below ${falseCounter} account(s):`);
+      console.log(
+        `Balances don't match for the below ${falseCounter} account(s):`
+      );
       for (let a in falseAccsList) {
         console.log(falseAccsList[a]);
       }
