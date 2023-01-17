063d215c9664187d27cbe45b2087f24e3c585b79
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-01-17 19:30:21 +0530

    Create handler systemKilledAccount

--- src/mappings/system/index.ts
+++ src/mappings/system/index.ts
@@ -3,7 +3,8 @@ import { EventHandlerContext } from '@subsquid/substrate-processor'
 import { Store } from '@subsquid/typeorm-store'
 import { Account, AccountBalance, HistoricalAccountBalance } from '../../model'
 import { getFees, initBalance } from '../helper'
-import { getExtrinsicFailedEvent, getExtrinsicSuccessEvent, getNewAccountEvent } from './types'
+import { getExtrinsicFailedEvent, getExtrinsicSuccessEvent, getKilledAccountEvent, 
+  getNewAccountEvent } from './types'
 
 
 export async function systemExtrinsicFailed(ctx: EventHandlerContext<Store>) {
@@ -118,6 +119,41 @@ export async function systemExtrinsicSuccess(ctx: EventHandlerContext<Store>) {
       await store.save<HistoricalAccountBalance>(dHab)
     }
   }
+}
+
+export async function systemKilledAccount(ctx: EventHandlerContext<Store, {event: {args: true}}>) {
+  const { store, block, event } = ctx
+  const { walletId } = getKilledAccountEvent(ctx)
+
+  let acc = await store.get(Account, { where: { accountId: walletId } })
+  if (!acc) return
+  acc.pvalue = 0
+  console.log(`[${event.name}] Saving account: ${JSON.stringify(acc, null, 2)}`)
+  await store.save<Account>(acc)
+
+  let ab = await store.findOneBy(AccountBalance, { account: { accountId: walletId }, assetId: 'Ztg' })
+  if (!ab) return
+  const oldBalance = ab.balance
+  const newBalance = BigInt(0)
+  ab.balance = newBalance
+  ab.value = Number(ab.balance)
+  console.log(`[${event.name}] Saving account balance: ${JSON.stringify(ab, null, 2)}`)
+  await store.save<AccountBalance>(ab)
+
+  let hab = new HistoricalAccountBalance()
+  hab.id = event.id + '-' + walletId.substring(walletId.length - 5)
+  hab.accountId = acc.accountId
+  hab.event = event.name.split('.')[1]
+  hab.assetId = ab.assetId
+  hab.dBalance = newBalance - oldBalance
+  hab.balance = newBalance
+  hab.dValue = Number(hab.dBalance)
+  hab.value = Number(hab.balance)
+  hab.pvalue = acc.pvalue
+  hab.blockNumber = block.height
+  hab.timestamp = new Date(block.timestamp)
+  console.log(`[${event.name}] Saving historical account balance: ${JSON.stringify(hab, null, 2)}`)
+  await store.save<HistoricalAccountBalance>(hab)
 }
 
 export async function systemNewAccount(ctx: EventHandlerContext<Store, {event: {args: true}}>) {
