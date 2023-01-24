5c0eb4dc7018c6d8360c0a104c3f4d73363daf15
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-01-24 16:13:09 +0530

    Create handler tokensBalanceSet

--- src/mappings/tokens/index.ts
+++ src/mappings/tokens/index.ts
@@ -2,9 +2,47 @@ import { EventHandlerContext, SubstrateEvent } from '@subsquid/substrate-process
 import { Store } from '@subsquid/typeorm-store'
 import { Account, AccountBalance, Asset, HistoricalAccountBalance } from '../../model'
 import { initBalance } from '../helper'
-import { getTokensDepositedEvent, getTokensEndowedEvent, getTokensTransferEvent, 
+import { getTokensBalanceSetEvent, getTokensDepositedEvent, getTokensEndowedEvent, getTokensTransferEvent, 
   getTokensWithdrawnEvent } from './types'
 
+
+export async function tokensBalanceSet(ctx: EventHandlerContext<Store, {event: {args: true}}>) {
+  const { store, block, event } = ctx 
+  const { assetId, walletId, amount } = getTokensBalanceSetEvent(ctx)
+
+  if (!assetId.includes(`pool`)) return
+  
+  let acc = await store.get(Account, { where: { accountId: walletId } })
+  if (!acc) return
+
+  let ab = await store.findOneBy(AccountBalance, { account: { accountId: walletId }, assetId: assetId })
+  let oldBalance = BigInt(0)
+  if (ab) {
+    oldBalance = ab.balance
+    ab.balance = amount
+  } else {
+    ab = new AccountBalance()
+    ab.id = event.id + '-' + walletId.substring(walletId.length - 5)
+    ab.account = acc
+    ab.assetId = assetId
+    ab.balance = amount
+  }
+  console.log(`[${event.name}] Saving account balance: ${JSON.stringify(ab, null, 2)}`)
+  await store.save<AccountBalance>(ab)
+
+  let hab = new HistoricalAccountBalance()
+  hab.id = event.id + '-' + walletId.substring(walletId.length - 5)
+  hab.accountId = acc.accountId
+  hab.event = event.name.split('.')[1]
+  hab.assetId = ab.assetId
+  hab.dBalance = ab.balance - oldBalance
+  hab.balance = ab.balance
+  hab.pvalue = acc.pvalue
+  hab.blockNumber = block.height
+  hab.timestamp = new Date(block.timestamp)
+  console.log(`[${event.name}] Saving historical account balance: ${JSON.stringify(hab, null, 2)}`)
+  await store.save<HistoricalAccountBalance>(hab)
+}
 
 export async function tokensDeposited(ctx: EventHandlerContext<Store, {event: {args: true}}>) {
   const { store, block, event } = ctx 
