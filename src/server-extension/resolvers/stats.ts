36ee6da160b475a47d77445fcd197c2cb38b24d2
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-02-16 09:56:18 +0530

    Add stats for total liquidity

---
+++ src/server-extension/resolvers/stats.ts
@@ -0,0 +1,26 @@
+import { Field, ObjectType, Query, Resolver } from 'type-graphql';
+import type { EntityManager } from 'typeorm';
+import { Market } from '../../model/generated';
+import { totalLiquidity } from '../query';
+
+@ObjectType()
+export class Stats {
+  @Field(() => BigInt)
+  totalLiquidity!: bigint;
+
+  constructor(props: Partial<Stats>) {
+    Object.assign(this, props);
+  }
+}
+
+@Resolver()
+export class StatsResolver {
+  constructor(private tx: () => Promise<EntityManager>) {}
+
+  @Query(() => [Stats])
+  async stats(): Promise<Stats[]> {
+    const manager = await this.tx();
+    const result = await manager.getRepository(Market).query(totalLiquidity());
+    return result;
+  }
+}
