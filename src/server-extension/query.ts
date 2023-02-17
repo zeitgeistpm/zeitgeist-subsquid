bdabbc08cb4aa6785298d86d1579024e4b5ff085
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2023-02-17 12:17:25 +0530

    Resolve total liquidity (#307)
    * Refactor query
    
    * Refactor market-stats
    
    * Add stats for total liquidity
    
    * Connect all resolvers
    
    * Reformat names

--- src/server-extension/query.ts
+++ src/server-extension/query.ts
@@ -1,7 +1,7 @@
 export const marketParticipants = (ids: string[]) => `
   SELECT
     m.market_id,
-    COALESCE(COUNT(DISTINCT ha.account_id), 0) as participants
+    COALESCE(COUNT(DISTINCT ha.account_id), 0) AS participants
   FROM
     market m
   LEFT JOIN
@@ -15,7 +15,7 @@ export const marketParticipants = (ids: string[]) => `
 export const marketLiquidity = (ids: string[]) => `
   SELECT
     m.market_id,
-    COALESCE(ROUND(SUM(a.price*a.amount_in_pool)+p.ztg_qty, 0), 0) as liquidity
+    COALESCE(ROUND(SUM(a.price*a.amount_in_pool)+p.ztg_qty, 0), 0) AS liquidity
   FROM
     market m
   JOIN
@@ -27,4 +27,21 @@ export const marketLiquidity = (ids: string[]) => `
   GROUP BY
     m.market_id,
     p.ztg_qty;
+`;
+
+export const totalLiquidity = () => `
+  SELECT
+    ROUND(SUM(liquidity),0) AS total_liquidity 
+  FROM (
+    SELECT
+      SUM(a.price*a.amount_in_pool)+p.ztg_qty AS liquidity
+    FROM
+      market m
+    JOIN
+      asset a ON a.asset_id = ANY (m.outcome_assets)
+    LEFT JOIN
+      pool p ON p.id = m.pool_id
+    GROUP BY
+      p.ztg_qty
+  ) AS market_liquidity;
 `;
