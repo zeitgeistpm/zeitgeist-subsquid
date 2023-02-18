01b371c9be0c352bd46605247a08e5bae7eb4504
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-02-18 14:46:41 +0530

    Adapt resolver to query changes

--- src/server-extension/resolvers/stats.ts
+++ src/server-extension/resolvers/stats.ts
@@ -1,12 +1,15 @@
 import { Field, ObjectType, Query, Resolver } from 'type-graphql';
 import type { EntityManager } from 'typeorm';
 import { Market } from '../../model/generated';
-import { totalLiquidity } from '../query';
+import { totalLiquidityAndVolume } from '../query';
 
 @ObjectType()
 export class Stats {
   @Field(() => BigInt, { name: 'totalLiquidity' })
   total_liquidity!: bigint;
+
+  @Field(() => BigInt, { name: 'totalVolume' })
+  total_volume!: bigint;
 
   constructor(props: Partial<Stats>) {
     Object.assign(this, props);
@@ -17,10 +20,10 @@ export class Stats {
 export class StatsResolver {
   constructor(private tx: () => Promise<EntityManager>) {}
 
-  @Query(() => [Stats])
-  async stats(): Promise<Stats[]> {
+  @Query(() => Stats)
+  async stats(): Promise<Stats> {
     const manager = await this.tx();
-    const result = await manager.getRepository(Market).query(totalLiquidity());
+    const result = await manager.getRepository(Market).query(totalLiquidityAndVolume());
     return result;
   }
 }
