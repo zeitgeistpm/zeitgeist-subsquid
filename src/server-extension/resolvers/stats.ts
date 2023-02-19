b7f8958d75d73bb2b5f291a8ef35e8e97f2a7aa5
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2023-02-19 12:30:50 +0530

    Resolve total volume (#308)
    * Update query for computing total-volume
    
    * Adapt resolver to query changes
    
    * Debug
    
    * Amend return type for stats query
    
    * Revert changes

--- src/server-extension/resolvers/stats.ts
+++ src/server-extension/resolvers/stats.ts
@@ -1,12 +1,15 @@
 import { Field, ObjectType, Query, Resolver } from 'type-graphql';
 import type { EntityManager } from 'typeorm';
 import { Market } from '../../model/generated';
-import { totalLiquidity } from '../query';
+import { totalLiquidityAndVolume } from '../query';
 
 @ObjectType()
 export class Stats {
   @Field(() => BigInt, { name: 'totalLiquidity' })
   total_liquidity!: bigint;
+
+  @Field(() => BigInt, { name: 'totalVolume' })
+  total_volume!: bigint;
 
   constructor(props: Partial<Stats>) {
     Object.assign(this, props);
@@ -20,7 +23,7 @@ export class StatsResolver {
   @Query(() => [Stats])
   async stats(): Promise<Stats[]> {
     const manager = await this.tx();
-    const result = await manager.getRepository(Market).query(totalLiquidity());
+    const result = await manager.getRepository(Market).query(totalLiquidityAndVolume());
     return result;
   }
 }
