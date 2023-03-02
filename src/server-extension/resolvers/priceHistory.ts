ac67262a5e682cf6f5fb39bb41c85e04d212e5e8
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2023-03-02 14:37:02 +0530

    Setup interval based asset prices (#315)
    * Add time input and output for interval based querying
    
    * Adapt resolver based on query changes
    
    * Update interval for interpolation
    
    * Catch all price changes using `1 SECOND`
    
    * Set start-time and end-time on corner cases

--- src/server-extension/resolvers/priceHistory.ts
+++ src/server-extension/resolvers/priceHistory.ts
@@ -1,8 +1,8 @@
 import { util } from '@zeitgeistpm/sdk';
 import { Arg, Field, ObjectType, Query, Resolver } from 'type-graphql';
 import { EntityManager } from 'typeorm';
-import { decodedAssetId, encodedAssetId, mergeByField } from '../helper';
-import { assetPriceHistory, outcomeAssets } from '../query';
+import { decodedAssetId, mergeByField } from '../helper';
+import { assetPriceHistory, marketInfo } from '../query';
 
 @ObjectType()
 export class PriceHistory {
@@ -10,7 +10,7 @@ export class PriceHistory {
   timestamp!: Date;
 
   @Field(() => [Price])
-  assets!: Price[];
+  prices!: Price[];
 
   constructor(props: Partial<PriceHistory>) {
     Object.assign(this, props);
@@ -22,7 +22,7 @@ class Price {
   @Field(() => String)
   assetId!: string;
 
-  @Field(() => Number)
+  @Field(() => Number, { nullable: true })
   price!: number;
 
   constructor(props: Partial<PriceHistory>) {
@@ -34,15 +34,35 @@ class Price {
 export class PriceHistoryResolver {
   constructor(private tx: () => Promise<EntityManager>) {}
 
-  @Query(() => [PriceHistory])
-  async priceHistory(@Arg('marketId', () => Number!, { nullable: false }) marketId: number): Promise<PriceHistory[]> {
+  @Query(() => [PriceHistory], { nullable: true })
+  async priceHistory(
+    @Arg('marketId', () => Number!, { nullable: false }) marketId: number,
+    @Arg('startTime', () => String, { nullable: true }) startTime: string,
+    @Arg('endTime', () => String, { nullable: true }) endTime: string,
+    @Arg('interval', { nullable: true, defaultValue: '1 DAY' }) interval: string
+  ): Promise<PriceHistory[] | undefined> {
     const manager = await this.tx();
-    const marketAssets = await manager.query(outcomeAssets(marketId));
+    const market = await manager.query(marketInfo(marketId));
+    if (!market[0]) return;
+
+    const poolCreateTime = market[0].timestamp.toISOString().replace('T', ' ').split('.')[0];
+    if (!startTime) startTime = poolCreateTime;
+    if (!endTime && market[1]) {
+      let marketResolvedTime = new Date(market[1].timestamp.toISOString());
+      marketResolvedTime.setDate(marketResolvedTime.getDate() + 1);
+      endTime = marketResolvedTime.toISOString().replace('T', ' ').split('.')[0];
+    } else {
+      endTime = new Date().toISOString().replace('T', ' ').split('.')[0];
+    }
 
     let merged = [];
-    let priceHistory = await manager.query(assetPriceHistory(marketAssets[0].assets[0]));
-    for (let i = 1; i < marketAssets[0].assets.length; i++) {
-      let priceHistory2 = await manager.query(assetPriceHistory(marketAssets[0].assets[i]));
+    let priceHistory = await manager.query(
+      assetPriceHistory(market[0].outcome_assets[0], poolCreateTime, startTime, endTime, interval)
+    );
+    for (let i = 1; i < market[0].outcome_assets.length; i++) {
+      let priceHistory2 = await manager.query(
+        assetPriceHistory(market[0].outcome_assets[i], poolCreateTime, startTime, endTime, interval)
+      );
       merged = mergeByField(priceHistory, priceHistory2, 'timestamp');
       priceHistory = merged;
     }
@@ -52,14 +72,14 @@ export class PriceHistoryResolver {
       let obj: any = {};
       obj.timestamp = merged[i].timestamp;
       delete merged[i].timestamp;
-      let assets = [];
+      let prices = [];
       for (const [key, value] of Object.entries(merged[i])) {
-        assets.push({
+        prices.push({
           assetId: JSON.stringify(util.AssetIdFromString(decodedAssetId(key))),
           price: value,
         });
       }
-      obj.assets = assets;
+      obj.prices = prices;
       result.push(obj);
     }
     return result;
