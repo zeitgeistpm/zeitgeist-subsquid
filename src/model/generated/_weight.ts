54f404770b67a6d91729096c3a75500b05e4d4b6
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-03-17 12:50:47 +0530

    Regenerate code

--- src/model/generated/_weight.ts
+++ src/model/generated/_weight.ts
@@ -5,45 +5,45 @@ import * as marshal from "./marshal"
  * Asset weightage details
  */
 export class Weight {
-  private _assetId!: string
-  private _len!: bigint
+    private _assetId!: string
+    private _len!: bigint
 
-  constructor(props?: Partial<Omit<Weight, 'toJSON'>>, json?: any) {
-    Object.assign(this, props)
-    if (json != null) {
-      this._assetId = marshal.string.fromJSON(json.assetId)
-      this._len = marshal.bigint.fromJSON(json.len)
+    constructor(props?: Partial<Omit<Weight, 'toJSON'>>, json?: any) {
+        Object.assign(this, props)
+        if (json != null) {
+            this._assetId = marshal.string.fromJSON(json.assetId)
+            this._len = marshal.bigint.fromJSON(json.len)
+        }
     }
-  }
 
-  /**
-   * Zeitgeist's identifier for asset
-   */
-  get assetId(): string {
-    assert(this._assetId != null, 'uninitialized access')
-    return this._assetId
-  }
-
-  set assetId(value: string) {
-    this._assetId = value
-  }
-
-  /**
-   * Length or weight of the asset
-   */
-  get len(): bigint {
-    assert(this._len != null, 'uninitialized access')
-    return this._len
-  }
-
-  set len(value: bigint) {
-    this._len = value
-  }
-
-  toJSON(): object {
-    return {
-      assetId: this.assetId,
-      len: marshal.bigint.toJSON(this.len),
+    /**
+     * Zeitgeist's identifier for asset
+     */
+    get assetId(): string {
+        assert(this._assetId != null, 'uninitialized access')
+        return this._assetId
     }
-  }
+
+    set assetId(value: string) {
+        this._assetId = value
+    }
+
+    /**
+     * Length or weight of the asset
+     */
+    get len(): bigint {
+        assert(this._len != null, 'uninitialized access')
+        return this._len
+    }
+
+    set len(value: bigint) {
+        this._len = value
+    }
+
+    toJSON(): object {
+        return {
+            assetId: this.assetId,
+            len: marshal.bigint.toJSON(this.len),
+        }
+    }
 }
