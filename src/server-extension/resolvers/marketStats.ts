08ef6a37ec183b419c2d39cdc9a11740f4843e41
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-04-26 07:37:26 +0530

    Update types for marketStats

--- src/server-extension/resolvers/marketStats.ts
+++ src/server-extension/resolvers/marketStats.ts
@@ -1,4 +1,4 @@
-import { Arg, Field, ObjectType, Query, Resolver } from 'type-graphql';
+import { Arg, Field, Int, ObjectType, Query, Resolver } from 'type-graphql';
 import type { EntityManager } from 'typeorm';
 import { Market } from '../../model/generated';
 import { mergeByField } from '../helper';
@@ -6,10 +6,10 @@ import { marketLiquidity, marketParticipants } from '../query';
 
 @ObjectType()
 export class MarketStats {
-  @Field(() => Number, { name: 'marketId' })
+  @Field(() => Int, { name: 'marketId' })
   market_id!: number;
 
-  @Field(() => Number)
+  @Field(() => Int)
   participants!: number;
 
   @Field(() => BigInt)
@@ -25,7 +25,7 @@ export class MarketStatsResolver {
   constructor(private tx: () => Promise<EntityManager>) {}
 
   @Query(() => [MarketStats])
-  async marketStats(@Arg('ids', () => [String!], { nullable: false }) ids: string[]): Promise<MarketStats[]> {
+  async marketStats(@Arg('marketId', () => [Int!], { nullable: false }) ids: number[]): Promise<MarketStats[]> {
     const manager = await this.tx();
     const participants = await manager.getRepository(Market).query(marketParticipants(ids));
     const liquidity = await manager.getRepository(Market).query(marketLiquidity(ids));
