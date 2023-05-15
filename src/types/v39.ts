8a5d0da63215652a85f86db2b71cb2cfb80b5a6e
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-05-15 14:30:00 +0530

    Regenerate types without `Balances.Endowed`

--- src/types/v39.ts
+++ src/types/v39.ts
@@ -1,85 +1,85 @@
-import type {Result} from './support'
+import type {Result, Option} from './support'
 
 export interface CommonPoolEventParams {
-  poolId: bigint
-  who: Uint8Array
+    poolId: bigint
+    who: Uint8Array
 }
 
 export interface Pool {
-  assets: Asset[]
-  baseAsset: Asset
-  marketId: bigint
-  poolStatus: PoolStatus
-  scoringRule: ScoringRule
-  swapFee: (bigint | undefined)
-  totalSubsidy: (bigint | undefined)
-  totalWeight: (bigint | undefined)
-  weights: ([Asset, bigint][] | undefined)
+    assets: Asset[]
+    baseAsset: Asset
+    marketId: bigint
+    poolStatus: PoolStatus
+    scoringRule: ScoringRule
+    swapFee: (bigint | undefined)
+    totalSubsidy: (bigint | undefined)
+    totalWeight: (bigint | undefined)
+    weights: ([Asset, bigint][] | undefined)
 }
 
 export type Asset = Asset_CategoricalOutcome | Asset_ScalarOutcome | Asset_CombinatorialOutcome | Asset_PoolShare | Asset_Ztg
 
 export interface Asset_CategoricalOutcome {
-  __kind: 'CategoricalOutcome'
-  value: [bigint, number]
+    __kind: 'CategoricalOutcome'
+    value: [bigint, number]
 }
 
 export interface Asset_ScalarOutcome {
-  __kind: 'ScalarOutcome'
-  value: [bigint, ScalarPosition]
+    __kind: 'ScalarOutcome'
+    value: [bigint, ScalarPosition]
 }
 
 export interface Asset_CombinatorialOutcome {
-  __kind: 'CombinatorialOutcome'
+    __kind: 'CombinatorialOutcome'
 }
 
 export interface Asset_PoolShare {
-  __kind: 'PoolShare'
-  value: bigint
+    __kind: 'PoolShare'
+    value: bigint
 }
 
 export interface Asset_Ztg {
-  __kind: 'Ztg'
+    __kind: 'Ztg'
 }
 
 export type PoolStatus = PoolStatus_Active | PoolStatus_CollectingSubsidy | PoolStatus_Closed | PoolStatus_Clean | PoolStatus_Initialized
 
 export interface PoolStatus_Active {
-  __kind: 'Active'
+    __kind: 'Active'
 }
 
 export interface PoolStatus_CollectingSubsidy {
-  __kind: 'CollectingSubsidy'
+    __kind: 'CollectingSubsidy'
 }
 
 export interface PoolStatus_Closed {
-  __kind: 'Closed'
+    __kind: 'Closed'
 }
 
 export interface PoolStatus_Clean {
-  __kind: 'Clean'
+    __kind: 'Clean'
 }
 
 export interface PoolStatus_Initialized {
-  __kind: 'Initialized'
+    __kind: 'Initialized'
 }
 
 export type ScoringRule = ScoringRule_CPMM | ScoringRule_RikiddoSigmoidFeeMarketEma
 
 export interface ScoringRule_CPMM {
-  __kind: 'CPMM'
+    __kind: 'CPMM'
 }
 
 export interface ScoringRule_RikiddoSigmoidFeeMarketEma {
-  __kind: 'RikiddoSigmoidFeeMarketEma'
+    __kind: 'RikiddoSigmoidFeeMarketEma'
 }
 
 export type ScalarPosition = ScalarPosition_Long | ScalarPosition_Short
 
 export interface ScalarPosition_Long {
-  __kind: 'Long'
+    __kind: 'Long'
 }
 
 export interface ScalarPosition_Short {
-  __kind: 'Short'
+    __kind: 'Short'
 }
--- src/types/v40.ts
+++ src/types/v40.ts
@@ -1,148 +1,148 @@
-import type {Result} from './support'
+import type {Result, Option} from './support'
 
 export interface Market {
-  creator: Uint8Array
-  creation: MarketCreation
-  creatorFee: number
-  oracle: Uint8Array
-  metadata: Uint8Array
-  marketType: MarketType
-  period: MarketPeriod
-  deadlines: Deadlines
-  scoringRule: ScoringRule
-  status: MarketStatus
-  report: (Report | undefined)
-  resolvedOutcome: (OutcomeReport | undefined)
-  disputeMechanism: MarketDisputeMechanism
+    creator: Uint8Array
+    creation: MarketCreation
+    creatorFee: number
+    oracle: Uint8Array
+    metadata: Uint8Array
+    marketType: MarketType
+    period: MarketPeriod
+    deadlines: Deadlines
+    scoringRule: ScoringRule
+    status: MarketStatus
+    report: (Report | undefined)
+    resolvedOutcome: (OutcomeReport | undefined)
+    disputeMechanism: MarketDisputeMechanism
 }
 
 export type MarketCreation = MarketCreation_Permissionless | MarketCreation_Advised
 
 export interface MarketCreation_Permissionless {
-  __kind: 'Permissionless'
+    __kind: 'Permissionless'
 }
 
 export interface MarketCreation_Advised {
-  __kind: 'Advised'
+    __kind: 'Advised'
 }
 
 export type MarketType = MarketType_Categorical | MarketType_Scalar
 
 export interface MarketType_Categorical {
-  __kind: 'Categorical'
-  value: number
+    __kind: 'Categorical'
+    value: number
 }
 
 export interface MarketType_Scalar {
-  __kind: 'Scalar'
-  value: RangeInclusive
+    __kind: 'Scalar'
+    value: RangeInclusive
 }
 
 export type MarketPeriod = MarketPeriod_Block | MarketPeriod_Timestamp
 
 export interface MarketPeriod_Block {
-  __kind: 'Block'
-  value: Range
+    __kind: 'Block'
+    value: Range
 }
 
 export interface MarketPeriod_Timestamp {
-  __kind: 'Timestamp'
-  value: Range
+    __kind: 'Timestamp'
+    value: Range
 }
 
 export interface Deadlines {
-  gracePeriod: bigint
-  oracleDuration: bigint
-  disputeDuration: bigint
+    gracePeriod: bigint
+    oracleDuration: bigint
+    disputeDuration: bigint
 }
 
 export type ScoringRule = ScoringRule_CPMM | ScoringRule_RikiddoSigmoidFeeMarketEma
 
 export interface ScoringRule_CPMM {
-  __kind: 'CPMM'
+    __kind: 'CPMM'
 }
 
 export interface ScoringRule_RikiddoSigmoidFeeMarketEma {
-  __kind: 'RikiddoSigmoidFeeMarketEma'
+    __kind: 'RikiddoSigmoidFeeMarketEma'
 }
 
 export type MarketStatus = MarketStatus_Proposed | MarketStatus_Active | MarketStatus_Suspended | MarketStatus_Closed | MarketStatus_CollectingSubsidy | MarketStatus_InsufficientSubsidy | MarketStatus_Reported | MarketStatus_Disputed | MarketStatus_Resolved
 
 export interface MarketStatus_Proposed {
-  __kind: 'Proposed'
+    __kind: 'Proposed'
 }
 
 export interface MarketStatus_Active {
-  __kind: 'Active'
+    __kind: 'Active'
 }
 
 export interface MarketStatus_Suspended {
-  __kind: 'Suspended'
+    __kind: 'Suspended'
 }
 
 export interface MarketStatus_Closed {
-  __kind: 'Closed'
+    __kind: 'Closed'
 }
 
 export interface MarketStatus_CollectingSubsidy {
-  __kind: 'CollectingSubsidy'
+    __kind: 'CollectingSubsidy'
 }
 
 export interface MarketStatus_InsufficientSubsidy {
-  __kind: 'InsufficientSubsidy'
+    __kind: 'InsufficientSubsidy'
 }
 
 export interface MarketStatus_Reported {
-  __kind: 'Reported'
+    __kind: 'Reported'
 }
 
 export interface MarketStatus_Disputed {
-  __kind: 'Disputed'
+    __kind: 'Disputed'
 }
 
 export interface MarketStatus_Resolved {
-  __kind: 'Resolved'
+    __kind: 'Resolved'
 }
 
 export interface Report {
-  at: bigint
-  by: Uint8Array
-  outcome: OutcomeReport
+    at: bigint
+    by: Uint8Array
+    outcome: OutcomeReport
 }
 
 export type OutcomeReport = OutcomeReport_Categorical | OutcomeReport_Scalar
 
 export interface OutcomeReport_Categorical {
-  __kind: 'Categorical'
-  value: number
+    __kind: 'Categorical'
+    value: number
 }
 
 export interface OutcomeReport_Scalar {
-  __kind: 'Scalar'
-  value: bigint
+    __kind: 'Scalar'
+    value: bigint
 }
 
 export type MarketDisputeMechanism = MarketDisputeMechanism_Authorized | MarketDisputeMechanism_Court | MarketDisputeMechanism_SimpleDisputes
 
 export interface MarketDisputeMechanism_Authorized {
-  __kind: 'Authorized'
-  value: Uint8Array
+    __kind: 'Authorized'
+    value: Uint8Array
 }
 
 export interface MarketDisputeMechanism_Court {
-  __kind: 'Court'
+    __kind: 'Court'
 }
 
 export interface MarketDisputeMechanism_SimpleDisputes {
-  __kind: 'SimpleDisputes'
+    __kind: 'SimpleDisputes'
 }
 
 export interface RangeInclusive {
-  start: bigint
-  end: bigint
+    start: bigint
+    end: bigint
 }
 
 export interface Range {
-  start: bigint
-  end: bigint
+    start: bigint
+    end: bigint
 }
