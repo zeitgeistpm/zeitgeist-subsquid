123f0a8c3301f6e29887e649e2b2bc31c2911003
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2023-05-18 09:02:49 +0530

    Cache market for which proposed alert has been posted (#368)
    * Add package nedb-promises
    
    * Init subscribe-db class and functions
    
    * Cache alerts issued for proposedMarkets

--- scripts/subscribe/proposedMarkets.ts
+++ scripts/subscribe/proposedMarkets.ts
@@ -5,6 +5,7 @@
 import { createClient } from 'graphql-ws';
 import WebSocket from 'ws';
 import axios from 'axios';
+import Db from './db';
 import { Market } from '../../src/model';
 
 const NODE_URL = process.argv[2];
@@ -12,17 +13,16 @@ const WEBHOOK_URL = process.argv[3];
 const GRAPHQL_WS_URL = NODE_URL.includes(`bs`)
   ? `wss://processor.bsr.zeitgeist.pm/graphql`
   : `wss://processor.rpc-0.zeitgeist.pm/graphql`;
+const CACHE_DB_PATH = `subscribe-db.db`;
 
-const client = createClient({
-  webSocketImpl: WebSocket,
-  url: GRAPHQL_WS_URL,
-});
+const client = createClient({ webSocketImpl: WebSocket, url: GRAPHQL_WS_URL });
+const db = new Db(CACHE_DB_PATH);
 
 client.subscribe(
   {
     query: `
       subscription {
-        markets(where: {status_eq: Proposed}, limit: 1, orderBy: marketId_DESC) {
+        markets(where: {status_eq: Proposed}, orderBy: marketId_DESC) {
           description
           marketId
           marketType {
@@ -39,11 +39,13 @@ client.subscribe(
     `,
   },
   {
-    next: ({ data }) => {
+    next: async ({ data }) => {
       const { markets } = data as any;
-      if (markets.length > 0) {
-        console.log(markets[0]);
-        postDiscordAlert(markets[0]);
+      for (let i = 0; i < markets.length; i++) {
+        const entry = await db.getMarketWithId(markets[i].marketId);
+        if (entry) break;
+        postDiscordAlert(markets[i]);
+        await db.saveOrUpdateMarket(markets[i].marketId, Date.now());
       }
     },
     error: (error) => {
@@ -56,6 +58,7 @@ client.subscribe(
 );
 
 const postDiscordAlert = async (market: Market) => {
+  console.log(`Posting alert for market: ${JSON.stringify(market, null, 2)}`);
   await axios.post(WEBHOOK_URL, {
     username: 'Market Proposed Alert',
     content: '',
