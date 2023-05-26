c54fd085a8667450be87dbf0f30481cf83fa187f
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-05-26 17:20:40 +0530

    Wrap error handling around axios request

--- scripts/subscribe/proposedMarkets.ts
+++ scripts/subscribe/proposedMarkets.ts
@@ -59,35 +59,41 @@ client.subscribe(
   }
 );
 
-const postDiscordAlert = async (market: Market) => {
+const postDiscordAlert = async (market: Market): Promise<AxiosResponse<any, any> | undefined> => {
   console.log(`Posting ${market.status} alert for marketId: ${market.marketId}`);
-  await axios.post(WEBHOOK_URL, {
-    username: 'Market Proposed Alert',
-    content: '',
-    embeds: [
-      {
-        color: '16766720',
-        title: market.question,
-        url: `https://app.zeitgeist.pm/markets/${market.marketId}`,
-        fields: [
-          {
-            name: 'Description',
-            value: market.description!.replace(/<[^>]+>/g, ''),
-          },
-          {
-            name: 'MarketType',
-            value: market.marketType.categorical
-              ? `Categorical : ${market.marketType.categorical}`
-              : `Scalar : ${market.marketType.scalar}`,
-          },
-          {
-            name: 'Period',
-            value: `${new Date(+market.period.start.toString()).toUTCString()} - ${new Date(
-              +market.period.end.toString()
-            ).toUTCString()}`,
-          },
-        ],
-      },
-    ],
-  });
+  try {
+    const res = await axios.post(WEBHOOK_URL, {
+      username: 'Market Proposed Alert',
+      content: '',
+      embeds: [
+        {
+          color: '16766720',
+          title: market.question,
+          url: `https://app.zeitgeist.pm/markets/${market.marketId}`,
+          fields: [
+            {
+              name: 'Description',
+              value: market.description!.replace(/<[^>]+>/g, ''),
+            },
+            {
+              name: 'MarketType',
+              value: market.marketType.categorical
+                ? `Categorical : ${market.marketType.categorical}`
+                : `Scalar : ${market.marketType.scalar}`,
+            },
+            {
+              name: 'Period',
+              value: `${new Date(+market.period.start.toString()).toUTCString()} - ${new Date(
+                +market.period.end.toString()
+              ).toUTCString()}`,
+            },
+          ],
+        },
+      ],
+    });
+    return res;
+  } catch (err) {
+    console.error(`Error while posting discord alert: ${err}`);
+    return;
+  }
 };
