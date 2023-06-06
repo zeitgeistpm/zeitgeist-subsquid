6242864c3c769e31d7599c111003031761216762
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-06-06 10:24:46 +0530

    Resolve at blockNum:167323

--- src/processor.ts
+++ src/processor.ts
@@ -306,6 +306,8 @@ const handlePostHooks = async (ctx: Ctx, block: SubstrateBlock) => {
       return;
     case 164057:
       return resolveMarket(ctx, block, 169, '1');
+    case 167323:
+      return resolveMarket(ctx, block, 224, '2');
     case 168378:
       await unreserveBalances_168378(ctx, block);
       await resolveMarket(ctx, block, 167, '0');
@@ -316,8 +318,6 @@ const handlePostHooks = async (ctx: Ctx, block: SubstrateBlock) => {
       await unreserveBalances_175178(ctx, block);
       await resolveMarket(ctx, block, 155, '0');
       return;
-    case 176396:
-      return resolveMarket(ctx, block, 224, '0');
     case 176408:
       await unreserveBalances_176408(ctx, block);
       await resolveMarket(ctx, block, 56, '2');
