b8e46b52fc732e817e1dfd54224b873e6c61de9a
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2023-06-22 06:45:43 +0530

    Add resolver to gather balance-history records for an account (#403)
    * Make values in categories and weights non-null
    
    * Adapt market and pool models
    
    * Purge poolStatus
    
    * Adapt to schema changes
    
    * Adapt swap handlers
    
    * Add query to fetch of sum of dBalance on and before blockNumber
    
    * Add resolver to map query

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,5 +1,6 @@
+import { BalanceInfoResolver } from './balanceInfo';
 import { MarketStatsResolver } from './marketStats';
 import { PriceHistoryResolver } from './priceHistory';
 import { StatsResolver } from './stats';
 
-export { MarketStatsResolver, PriceHistoryResolver, StatsResolver };
+export { BalanceInfoResolver, MarketStatsResolver, PriceHistoryResolver, StatsResolver };
