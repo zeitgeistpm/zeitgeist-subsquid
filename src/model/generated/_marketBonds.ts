340c3e52fc52829cbb81c8711ae211e7d6f3c90d
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-06-27 16:08:58 +0530

    Make outsider field nullable

--- src/model/generated/_marketBonds.ts
+++ src/model/generated/_marketBonds.ts
@@ -10,14 +10,14 @@ import {MarketBond} from "./_marketBond"
 export class MarketBonds {
     private _creation!: MarketBond
     private _oracle!: MarketBond
-    private _outsider!: MarketBond
+    private _outsider!: MarketBond | undefined | null
 
     constructor(props?: Partial<Omit<MarketBonds, 'toJSON'>>, json?: any) {
         Object.assign(this, props)
         if (json != null) {
             this._creation = new MarketBond(undefined, marshal.nonNull(json.creation))
             this._oracle = new MarketBond(undefined, marshal.nonNull(json.oracle))
-            this._outsider = new MarketBond(undefined, marshal.nonNull(json.outsider))
+            this._outsider = json.outsider == null ? undefined : new MarketBond(undefined, json.outsider)
         }
     }
 
@@ -48,12 +48,11 @@ export class MarketBonds {
     /**
      * A bond for an outcome reporter, who is not the oracle
      */
-    get outsider(): MarketBond {
-        assert(this._outsider != null, 'uninitialized access')
+    get outsider(): MarketBond | undefined | null {
         return this._outsider
     }
 
-    set outsider(value: MarketBond) {
+    set outsider(value: MarketBond | undefined | null) {
         this._outsider = value
     }
 
@@ -61,7 +60,7 @@ export class MarketBonds {
         return {
             creation: this.creation.toJSON(),
             oracle: this.oracle.toJSON(),
-            outsider: this.outsider.toJSON(),
+            outsider: this.outsider == null ? undefined : this.outsider.toJSON(),
         }
     }
 }
