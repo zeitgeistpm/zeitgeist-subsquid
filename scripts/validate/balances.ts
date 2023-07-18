1dc54470392b1b709737c3445e84a39edd8d4960
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-07-18 11:42:27 +0530

    Reformat script and count validated accounts

--- scripts/validate/balances.ts
+++ scripts/validate/balances.ts
@@ -31,15 +31,16 @@ const validateBalances = async () => {
   const res = await axios.post(`https://${GRAPHQL_HOSTNAME}/graphql`, query);
   const accounts = res.data.data.accounts as Account[];
   const squidHeight = res.data.data.squidStatus.height as bigint;
+  console.log(`Received ${accounts.length} accounts at #${squidHeight} via ${GRAPHQL_HOSTNAME}`);
 
-  const { outlierMap, validatedCount } = await getOutliers(accounts, squidHeight);
-  console.log(`\nAccounts received via ${GRAPHQL_HOSTNAME}: ${accounts.length}`);
-  console.log(`Total asset balances validated: ${validatedCount}`);
-  if (validatedCount === 0) process.exit();
+  const { validatedAccCount, outlierMap, validatedABCount } = await getOutliers(accounts, squidHeight);
+  console.log(`\nTotal accounts validated: ${validatedAccCount}`);
+  console.log(`Total asset balances validated: ${validatedABCount}`);
+  if (validatedABCount === 0) process.exit();
   if (outlierMap.size > 0) {
     console.log(`Asset balances don't match for ${outlierMap.size} account(s) 🔴`);
     [...outlierMap.entries()].map(([accountId, assets], idx) => {
-      console.log(`${idx + 1}. ` + accountId);
+      console.log(`${idx + 1}. ${accountId}`);
       console.log(assets);
     });
     process.exit();
@@ -51,26 +52,24 @@ const validateBalances = async () => {
 const getOutliers = async (
   accounts: Account[],
   squidHeight: bigint
-): Promise<{ outlierMap: Map<string, string[]>; validatedCount: number }> => {
-  let validatedCount = 0;
+): Promise<{ validatedAccCount: number; outlierMap: Map<string, string[]>; validatedABCount: number }> => {
+  let validatedAccCount = 0,
+    validatedABCount = 0;
   const outlierMap = new Map<string, string[]>();
   const sdk = await Tools.getSDK(NODE_URL);
   const blockHash = await sdk.api.rpc.chain.getBlockHash(squidHeight);
+
   await Promise.all(
     accounts.map(async (account) => {
       await Promise.all(
         account.balances.map(async (ab) => {
+          let chainBal;
           try {
             if (ab.assetId === 'Ztg') {
               const {
                 data: { free },
               } = (await sdk.api.query.system.account.at(blockHash, account.accountId)) as AccountInfo;
-              if (!isSame(account.accountId, free, ab)) {
-                let assets = outlierMap.get(account.accountId) || [];
-                assets.push(ab.assetId);
-                outlierMap.set(account.accountId, assets);
-              }
-              validatedCount++;
+              chainBal = free;
             } else {
               const { free } = (await sdk.api.query.tokens.accounts.at(
                 blockHash,
@@ -79,22 +78,24 @@ const getOutliers = async (
                   ? { ForeignAsset: ab.assetId.replace(/\D/g, '') }
                   : util.AssetIdFromString(ab.assetId)
               )) as any;
-              if (!isSame(account.accountId, free, ab)) {
-                let assets = outlierMap.get(account.accountId) || [];
-                assets.push(ab.assetId);
-                outlierMap.set(account.accountId, assets);
-              }
-              validatedCount++;
+              chainBal = free;
             }
           } catch (err) {
             console.error(err);
-            return { outlierMap, validatedCount };
+            return { validatedAccCount, outlierMap, validatedABCount };
           }
+          if (!isSame(account.accountId, chainBal, ab)) {
+            let assets = outlierMap.get(account.accountId) || [];
+            assets.push(ab.assetId);
+            outlierMap.set(account.accountId, assets);
+          }
+          validatedABCount++;
         })
       );
+      validatedAccCount++;
     })
   );
-  return { outlierMap, validatedCount };
+  return { validatedAccCount, outlierMap, validatedABCount };
 };
 
 const isSame = (accountId: string, chainBal: any, squidAB: AccountBalance): boolean => {
