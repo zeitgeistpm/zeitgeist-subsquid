68a445a0ba6a2799660e5868a3266ebed9f746b3
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2023-09-01 12:08:29 +0530

    Process & map AssetTxPaymentAssetTxFeePaidEvent (#431)
    * Generate types for assetTxPayment and assetMetadata
    
    * Init AssetTxPaymentAssetTxFeePaidEvent
    
    * Setup event handler assetTxPaymentAssetTxFeePaidEvent
    
    * Pull metadata for new specVersions
    
    * Connect handler to processor
    
    * Init types for AssetRegistryMetadataStorage
    
    * Debug assetMetadataStorage for specVersion:47
    
    * Debug getAssetTxFeePaidEvent for specVersion:47
    
    * Review assetId representations
    
    * Round assetTxFeePaid

---
+++ src/mappings/assetTxPayment/index.ts
@@ -0,0 +1,41 @@
+import { SubstrateBlock } from '@subsquid/substrate-processor';
+import { Ctx, EventItem } from '../../processor';
+import { Account, HistoricalAccountBalance } from '../../model';
+import { Asset_ForeignAsset } from '../../types/v48';
+import { extrinsicFromEvent, getAssetId, initBalance } from '../helper';
+import { getAssetTxFeePaidEvent, getMetadataStorage } from './types';
+
+export const assetTxPaymentAssetTxFeePaidEvent = async (
+  ctx: Ctx,
+  block: SubstrateBlock,
+  item: EventItem
+): Promise<HistoricalAccountBalance | undefined> => {
+  const { walletId, actualFee, assetId } = getAssetTxFeePaidEvent(ctx, item);
+  const currencyId: Asset_ForeignAsset = { __kind: 'ForeignAsset', value: assetId };
+
+  let acc = await ctx.store.get(Account, { where: { accountId: walletId } });
+  if (!acc) {
+    acc = new Account();
+    acc.id = walletId;
+    acc.accountId = walletId;
+    console.log(`[${item.event.name}] Saving account: ${JSON.stringify(acc, null, 2)}`);
+    await ctx.store.save<Account>(acc);
+    await initBalance(acc, ctx.store, block, item);
+  }
+
+  const onChainAsset = await getMetadataStorage(ctx, block, currencyId);
+  if (!onChainAsset || !onChainAsset.additional.xcm.feeFactor) return;
+  const amount = (Number(actualFee) * Number(onChainAsset.additional.xcm.feeFactor)) / 10 ** 10;
+
+  const hab = new HistoricalAccountBalance();
+  hab.id = item.event.id + '-' + walletId.substring(walletId.length - 5);
+  hab.accountId = acc.accountId;
+  hab.event = item.event.name.split('.')[1];
+  hab.extrinsic = extrinsicFromEvent(item.event);
+  hab.assetId = getAssetId(currencyId);
+  hab.dBalance = -BigInt(Math.round(amount));
+  hab.blockNumber = block.height;
+  hab.timestamp = new Date(block.timestamp);
+
+  return hab;
+};
---
+++ src/mappings/assetTxPayment/types.ts
@@ -0,0 +1,47 @@
+import { encodeAddress } from '@polkadot/keyring';
+import { SubstrateBlock } from '@subsquid/substrate-processor';
+import * as ss58 from '@subsquid/ss58';
+import { Ctx, EventItem } from '../../processor';
+import { AssetTxPaymentAssetTxFeePaidEvent } from '../../types/events';
+import { AssetRegistryMetadataStorage } from '../../types/storage';
+import { AssetMetadata, Asset_ForeignAsset } from '../../types/v48';
+
+export const getAssetTxFeePaidEvent = (ctx: Ctx, item: EventItem): AssetTxFeePaidEvent => {
+  // @ts-ignore
+  const event = new AssetTxPaymentAssetTxFeePaidEvent(ctx, item.event);
+  if (event.isV47) {
+    let { who, actualFee, assetId } = event.asV47;
+    const walletId = ss58.codec('zeitgeist').encode(who);
+    assetId = (assetId as any).value;
+    return { walletId, actualFee, assetId };
+  } else if (event.isV48) {
+    const { who, actualFee, assetId } = event.asV48;
+    const walletId = ss58.codec('zeitgeist').encode(who);
+    return { walletId, actualFee, assetId };
+  } else {
+    const { who, actualFee, assetId } = item.event.args;
+    const walletId = encodeAddress(who, 73);
+    return { walletId, actualFee, assetId };
+  }
+};
+
+export const getMetadataStorage = async (
+  ctx: Ctx,
+  block: SubstrateBlock,
+  assetId: Asset_ForeignAsset
+): Promise<AssetMetadata | undefined> => {
+  const storage = new AssetRegistryMetadataStorage(ctx, block);
+  if (storage.isV42) {
+    const assetMetadata = await storage.asV42.get(assetId);
+    return assetMetadata;
+  } else if (storage.isV48) {
+    const assetMetadata = await storage.asV48.get(assetId);
+    return assetMetadata;
+  }
+};
+
+interface AssetTxFeePaidEvent {
+  walletId: string;
+  actualFee: bigint;
+  assetId: any;
+}
