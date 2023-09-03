13a4f3b56410ade1440378add344e3e669db4557
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2023-09-03 12:41:47 +0530

    Validate asset balance history of all squid accounts (#433)
    * Accept 3 more inputs to determind kind and value of asset
    
    * Adapt queries to accept all assets
    
    * Adapt validation logic to new changes
    
    * Reformat comments
    
    * Stop validating ScalarOutcome asset

--- scripts/validate/balanceHistory.ts
+++ scripts/validate/balanceHistory.ts
@@ -1,10 +1,10 @@
 /**
- * Script to validate Ztg balance history of an account against on-chain data
- * Run using the command: ts-node scripts/validate/balanceHistory.ts <env> <account-id>
+ * Script to validate asset balance history of an account against on-chain data
+ * Run using the command: ts-node scripts/validate/balanceHistory.ts <env> <account-id> <asset-kind> <asset-value1> <asset-value2>
  */
 import axios from 'axios';
 import { AccountInfo } from '@polkadot/types/interfaces/system';
-import SDK from '@zeitgeistpm/sdk';
+import SDK, { util } from '@zeitgeistpm/sdk';
 import { Tools } from '../../src/mappings/util';
 import { HistoricalAccountBalance } from '../../src/model';
 
@@ -27,16 +27,52 @@ switch (CHAIN_ENV) {
   default:
     console.log(`Please pass 'dev' or 'test' or 'main' as environment`);
     console.log(
-      `Example: ts-node scripts/validate/balanceHistory.ts main dDyQQkwy5MmibHv5y1qQYpADS6x2x8yJjzaTRt2uTGfeSD7V9`
+      `Example: ts-node scripts/validate/balanceHistory.ts dev dE3phcNpdXRzK8RQnWej9C87CJ3AphKwCYXniTKAqiaUfdfCz PoolShare 316`
     );
     process.exit(1);
 }
 
 const ACCOUNT_ID = process.argv[3];
+const ASSET_KIND = process.argv[4];
+const ASSET_VALUE1 = process.argv[5];
+const ASSET_VALUE2 = process.argv[6];
+
+let chainAssetId: any,
+  assetValue: string | null,
+  assetQuery = '';
+switch (ASSET_KIND) {
+  case 'CategoricalOutcome':
+    assetValue = `${ASSET_VALUE1},${ASSET_VALUE2}`;
+    assetQuery = `assetId_contains: "[${assetValue}]"`;
+    chainAssetId = util.AssetIdFromString(`[${assetValue}]`);
+    break;
+  case 'ForeignAsset':
+    assetValue = ASSET_VALUE1;
+    assetQuery = `assetId_contains: "foreign", AND: {assetId_contains: "${assetValue}"}`;
+    chainAssetId = { ForeignAsset: ASSET_VALUE1 };
+    break;
+  case 'PoolShare':
+    assetValue = ASSET_VALUE1;
+    assetQuery = `assetId_contains: "pool", AND: {assetId_contains: "${assetValue}"}`;
+    chainAssetId = util.AssetIdFromString(`pool ${assetValue}`);
+    break;
+  case 'Ztg':
+    assetValue = null;
+    assetQuery = `assetId_eq: "${ASSET_KIND}"`;
+    chainAssetId = ASSET_KIND;
+    break;
+  default:
+    console.log(`Please pass CategoricalOutcome/ForeignAsset/PoolShare/ScalarOutcome/Ztg as asset-kind`);
+    console.log(
+      `Example: ts-node scripts/validate/balanceHistory.ts dev dE3phcNpdXRzK8RQnWej9C87CJ3AphKwCYXniTKAqiaUfdfCz CategoricalOutcome 367 0`
+    );
+    process.exit(1);
+}
+
 const balanceHistoryQuery = {
   // GraphQL query for retrieving balance history of an account
   query: `{
-    historicalAccountBalances(where: {accountId_eq: "${ACCOUNT_ID}", assetId_eq: "Ztg"}, orderBy: blockNumber_ASC) {
+    historicalAccountBalances(where: {accountId_eq: "${ACCOUNT_ID}", ${assetQuery}}, orderBy: blockNumber_ASC) {
       blockNumber
     }
   }`,
@@ -45,35 +81,47 @@ const balanceHistoryQuery = {
 const validateBalanceHistory = async () => {
   const res = await axios.post(`https://${GRAPHQL_HOSTNAME}/graphql`, balanceHistoryQuery);
   const balanceHistory = res.data.data.historicalAccountBalances as HistoricalAccountBalance[];
-  console.log(`Received ${balanceHistory.length} records of ${ACCOUNT_ID} via ${GRAPHQL_HOSTNAME}`);
+  console.log(
+    `Received ${balanceHistory.length} records of ${JSON.stringify(
+      chainAssetId
+    )} on ${ACCOUNT_ID} via ${GRAPHQL_HOSTNAME}`
+  );
+  if (balanceHistory.length === 0) process.exit(0);
 
   const sdk = await Tools.getSDK(NODE_URL);
   let fromBlockNum = balanceHistory[0].blockNumber;
-  let toBlockNum = balanceHistory[balanceHistory.length - 1].blockNumber;
-  let lastDiff = BigInt(0);
+  let toBlockNum = balanceHistory[balanceHistory.length - 1].blockNumber + 1;
+  let lastChainBalance = BigInt(0);
+  let lastSquidBalance = BigInt(0);
 
   // Inspired by binary search algorithm
-  while (fromBlockNum <= toBlockNum) {
+  while (fromBlockNum < toBlockNum) {
     console.log(`Reading between #${fromBlockNum} and #${toBlockNum}`);
     const midBlockNum = Math.floor((fromBlockNum + toBlockNum) / 2);
     const chainBalance = await getChainBalance(sdk, midBlockNum);
     const squidBalance = await getSquidBalance(midBlockNum);
 
-    console.log(`Detected difference of ${squidBalance - chainBalance} ZTG at #${midBlockNum}`);
-    if (squidBalance - chainBalance !== BigInt(0)) {
+    console.log(`Detected difference of ${squidBalance - chainBalance} units at #${midBlockNum}`);
+    if (squidBalance !== chainBalance) {
       toBlockNum = midBlockNum;
-      lastDiff = squidBalance - chainBalance;
+      lastChainBalance = chainBalance;
+      lastSquidBalance = squidBalance;
     } else {
       fromBlockNum = midBlockNum;
     }
 
     // When all block numbers are covered
     if (toBlockNum - fromBlockNum === 1) {
-      if (lastDiff !== BigInt(0)) {
-        console.log(`\nFound difference of ${lastDiff} ZTG on ${ACCOUNT_ID} at #${toBlockNum}`);
-        console.log(`On Subsquid: ${squidBalance}, On Chain: ${chainBalance}`);
+      console.log();
+      if (lastSquidBalance !== lastChainBalance) {
+        console.log(
+          `Found difference of ${lastSquidBalance - lastChainBalance} units on ${ACCOUNT_ID} at #${toBlockNum}`
+        );
+        console.log(`On Subsquid: ${lastSquidBalance}, On Chain: ${lastChainBalance}`);
       } else {
-        console.log(`Ztg balance history of ${ACCOUNT_ID} is in sync with ${CHAIN_ENV} chain`);
+        console.log(
+          `${JSON.stringify(chainAssetId)} balance history of ${ACCOUNT_ID} is in sync with ${CHAIN_ENV} chain`
+        );
       }
       sdk.api.disconnect();
       process.exit(0);
@@ -81,11 +129,11 @@ const validateBalanceHistory = async () => {
   }
 };
 
-// To query indexer for Ztg balance as on block number
+// To query indexer for asset balance as on block number
 const getSquidBalance = async (blockNumber: number): Promise<bigint> => {
   const res = await axios.post(`https://${GRAPHQL_HOSTNAME}/graphql`, {
     query: `{
-      balanceInfo(accountId: "${ACCOUNT_ID}", blockNumber: "${blockNumber}") {
+      balanceInfo(accountId: "${ACCOUNT_ID}", assetId: {kind: ${ASSET_KIND}, value: "${assetValue}"}, blockNumber: "${blockNumber}") {
         balance
       }
     }`,
@@ -94,13 +142,22 @@ const getSquidBalance = async (blockNumber: number): Promise<bigint> => {
   return BigInt(balance);
 };
 
-// To query chain for Ztg balance as on block number
+// To query chain for asset balance as on block number
 const getChainBalance = async (sdk: SDK, blockNumber: number) => {
   const blockHash = await sdk.api.rpc.chain.getBlockHash(blockNumber);
-  const {
-    data: { free: amt },
-  } = (await sdk.api.query.system.account.at(blockHash, ACCOUNT_ID)) as AccountInfo;
-  return amt.toBigInt();
+  try {
+    if (ASSET_KIND === 'Ztg') {
+      const {
+        data: { free },
+      } = (await sdk.api.query.system.account.at(blockHash, ACCOUNT_ID)) as AccountInfo;
+      return free.toBigInt();
+    } else {
+      const { free } = (await sdk.api.query.tokens.accounts.at(blockHash, ACCOUNT_ID, chainAssetId)) as any;
+      return free.toBigInt();
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 validateBalanceHistory();
