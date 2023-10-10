3179935e5e454b8fc6e4b693a3debd6f8800217e
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-10-10 19:13:29 +0530

    Setup MarketMetadata resolver

---
+++ src/server-extension/resolvers/marketMetadata.ts
@@ -0,0 +1,43 @@
+import { Arg, Field, Int, ObjectType, Query, Resolver } from 'type-graphql';
+import type { EntityManager } from 'typeorm';
+import { Market } from '../../model/generated';
+import { Cache } from '../../mappings/util';
+import { marketMetadata } from '../query';
+
+@ObjectType()
+export class MarketMetadata {
+  @Field(() => Int)
+  marketId!: number;
+
+  @Field(() => String)
+  encoded!: string;
+
+  @Field(() => String, { nullable: true })
+  decoded!: string;
+
+  constructor(props: Partial<MarketMetadata>) {
+    Object.assign(this, props);
+  }
+}
+
+@Resolver()
+export class MarketMetadataResolver {
+  constructor(private tx: () => Promise<EntityManager>) {}
+
+  @Query(() => [MarketMetadata])
+  async marketMetadata(@Arg('marketId', () => [Int!], { nullable: false }) ids: number[]): Promise<MarketMetadata[]> {
+    const manager = await this.tx();
+    const encodedData = await manager.getRepository(Market).query(marketMetadata(ids));
+
+    let result: MarketMetadata[] = await Promise.all(
+      encodedData.map(async (ed: any) => {
+        return {
+          marketId: ed.market_id,
+          encoded: ed.metadata,
+          decoded: await (await Cache.init()).getMeta(ed.metadata),
+        };
+      })
+    );
+    return result;
+  }
+}
