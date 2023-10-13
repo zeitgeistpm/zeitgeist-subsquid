0530da162e007c90cd305cf2b8b093894dcabbb5
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-10-13 16:52:45 +0530

    Allow nullable outcome in MarketReport

--- src/model/generated/_marketReport.ts
+++ src/model/generated/_marketReport.ts
@@ -8,14 +8,14 @@ import {OutcomeReport} from "./_outcomeReport"
 export class MarketReport {
     private _at!: number | undefined | null
     private _by!: string | undefined | null
-    private _outcome!: OutcomeReport
+    private _outcome!: OutcomeReport | undefined | null
 
     constructor(props?: Partial<Omit<MarketReport, 'toJSON'>>, json?: any) {
         Object.assign(this, props)
         if (json != null) {
             this._at = json.at == null ? undefined : marshal.int.fromJSON(json.at)
             this._by = json.by == null ? undefined : marshal.string.fromJSON(json.by)
-            this._outcome = new OutcomeReport(undefined, marshal.nonNull(json.outcome))
+            this._outcome = json.outcome == null ? undefined : new OutcomeReport(undefined, json.outcome)
         }
     }
 
@@ -44,12 +44,11 @@ export class MarketReport {
     /**
      * Outcome details
      */
-    get outcome(): OutcomeReport {
-        assert(this._outcome != null, 'uninitialized access')
+    get outcome(): OutcomeReport | undefined | null {
         return this._outcome
     }
 
-    set outcome(value: OutcomeReport) {
+    set outcome(value: OutcomeReport | undefined | null) {
         this._outcome = value
     }
 
@@ -57,7 +56,7 @@ export class MarketReport {
         return {
             at: this.at,
             by: this.by,
-            outcome: this.outcome.toJSON(),
+            outcome: this.outcome == null ? undefined : this.outcome.toJSON(),
         }
     }
 }
