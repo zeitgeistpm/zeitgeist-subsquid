56c61346dcc4f4e8238f1fe1497edc3746545b64
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-10-22 16:50:16 +0530

    Create txn for treasury account

--- src/mappings/assetTxPayment/index.ts
+++ src/mappings/assetTxPayment/index.ts
@@ -9,23 +9,37 @@ export const assetTxPaymentAssetTxFeePaidEvent = async (
   ctx: Ctx,
   block: SubstrateBlock,
   item: EventItem
-): Promise<HistoricalAccountBalance | undefined> => {
+): Promise<HistoricalAccountBalance[] | undefined> => {
   const { walletId, actualFee, assetId } = getAssetTxFeePaidEvent(ctx, item);
   const currencyId: Asset_ForeignAsset = { __kind: 'ForeignAsset', value: assetId };
+  const habs: HistoricalAccountBalance[] = [];
 
   const onChainAsset = await getMetadataStorage(ctx, block, currencyId);
   if (!onChainAsset || !onChainAsset.additional.xcm.feeFactor) return;
   const amount = (Number(actualFee) * Number(onChainAsset.additional.xcm.feeFactor)) / 10 ** 10;
 
-  const hab = new HistoricalAccountBalance();
-  hab.id = item.event.id + '-' + walletId.slice(-5);
-  hab.accountId = walletId;
-  hab.event = item.event.name.split('.')[1];
-  hab.extrinsic = extrinsicFromEvent(item.event);
-  hab.assetId = getAssetId(currencyId);
-  hab.dBalance = -BigInt(Math.round(amount));
-  hab.blockNumber = block.height;
-  hab.timestamp = new Date(block.timestamp);
+  const userHab: HistoricalAccountBalance = {
+    id: item.event.id + '-' + walletId.slice(-5),
+    accountId: walletId,
+    assetId: getAssetId(currencyId),
+    blockNumber: block.height,
+    dBalance: -BigInt(Math.round(amount)),
+    event: item.event.name.split('.')[1],
+    extrinsic: extrinsicFromEvent(item.event),
+    timestamp: new Date(block.timestamp),
+  };
 
-  return hab;
+  const treasuryHab: HistoricalAccountBalance = {
+    id: item.event.id + '-' + walletId.slice(-5),
+    accountId: 'dE1VdxVn8xy7HFQG5y5px7T2W1TDpRq1QXHH2ozfZLhBMYiBJ',
+    assetId: getAssetId(currencyId),
+    blockNumber: block.height,
+    dBalance: BigInt(Math.round(amount)),
+    event: item.event.name.split('.')[1],
+    extrinsic: extrinsicFromEvent(item.event),
+    timestamp: new Date(block.timestamp),
+  };
+
+  habs.push(userHab, treasuryHab);
+  return habs;
 };
