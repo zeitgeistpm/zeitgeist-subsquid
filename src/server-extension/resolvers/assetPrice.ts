a654be1f52026e414600be0c603035a5b97ba45b
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-10-31 03:26:49 +0530

    Connect redis-db to AssetPriceResolver

--- src/server-extension/resolvers/assetPrice.ts
+++ src/server-extension/resolvers/assetPrice.ts
@@ -1,6 +1,8 @@
 import { Arg, Field, ObjectType, Query, Resolver, registerEnumType } from 'type-graphql';
 import { EntityManager } from 'typeorm';
 import axios from 'axios';
+import { CacheHint, EPOCH_TIME, TEN_MINUTES } from '../../mappings/helper';
+import { Cache } from '../../mappings/util';
 import { HistoricalMarket } from '../../model';
 
 @ObjectType()
@@ -8,8 +10,8 @@ export class AssetPrice {
   @Field(() => String)
   pair!: string;
 
-  @Field(() => Number)
-  price!: number;
+  @Field(() => Number, { nullable: true })
+  price!: number | undefined;
 
   @Field(() => Date)
   timestamp!: Date;
@@ -38,14 +40,22 @@ registerEnumType(DenominatorAsset, {
 
 @Resolver()
 export class AssetPriceResolver {
+  static cachedAt: Date = EPOCH_TIME;
+
   constructor(private tx: () => Promise<EntityManager>) {}
 
   @Query(() => [AssetPrice])
   async assetPrice(
     @Arg('assetId', () => [NumeratorAsset], { nullable: false }) numerator: NumeratorAsset[],
     @Arg('denominator', () => [DenominatorAsset], { nullable: false }) denominator: DenominatorAsset[]
-  ): Promise<AssetPrice[] | null> {
+  ): Promise<AssetPrice[]> {
+    // Required for visibility on graphql
     const result = (await this.tx()).getRepository(HistoricalMarket);
+
+    // Refresh prices if they are older than 10 minutes
+    if (new Date().getTime() - AssetPriceResolver.cachedAt.getTime() > TEN_MINUTES) {
+      refreshPrices();
+    }
 
     // Remove duplicates
     const nums = [...new Set(numerator)];
@@ -54,16 +64,46 @@ export class AssetPriceResolver {
     const prices: AssetPrice[] = [];
     for (let i = 0; i < nums.length; i++) {
       for (let j = 0; j < dens.length; j++) {
-        const res = await axios.get(
-          `https://api.coingecko.com/api/v3/simple/price?ids=${nums[i]}&vs_currencies=${dens[j]}`
-        );
+        const pairedAsset = generatePair(nums[i], dens[j]);
+        const price = await fetchFromCache(pairedAsset);
         prices.push({
-          pair: `${nums[i]}/${dens[j]}`,
-          price: res.data[`${nums[i]}`][`${dens[j]}`],
-          timestamp: new Date(),
+          pair: pairedAsset,
+          price: price ?? undefined,
+          timestamp: AssetPriceResolver.cachedAt,
         });
       }
     }
     return prices;
   }
 }
+
+// Fetch and store prices for all supported assets from Coingecko
+const refreshPrices = async () => {
+  const ids = Object.values(NumeratorAsset).join(',');
+  const vs_currencies = Object.values(DenominatorAsset).join(',');
+  try {
+    const res = await axios.get(
+      `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=${vs_currencies}`
+    );
+    Object.entries(res.data).forEach(([numAsset, denAssetPrice]) => {
+      Object.entries(denAssetPrice as Map<string, number>).forEach(async ([denAsset, price]) => {
+        await (await Cache.init()).setData(CacheHint.Price, generatePair(numAsset, denAsset), price.toString());
+      });
+    });
+    AssetPriceResolver.cachedAt = new Date();
+  } catch (err) {
+    console.error(`Error while fetching prices from coingecko: ` + JSON.stringify(err, null, 2));
+  }
+};
+
+// Fetch price from redis db.. Returns null in case of failure
+const fetchFromCache = async (pair: string): Promise<number | null> => {
+  const price = await (await Cache.init()).getData(CacheHint.Price, pair);
+  return price ? +price : null;
+};
+
+const generatePair = (num: string, den: string): string => {
+  const numerator = Object.keys(NumeratorAsset)[Object.values(NumeratorAsset).indexOf(<any>num)];
+  const denominator = Object.keys(DenominatorAsset)[Object.values(DenominatorAsset).indexOf(<any>den)];
+  return `${numerator}/${denominator}`;
+};
