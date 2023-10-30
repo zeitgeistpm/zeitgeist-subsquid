6bc6c5a0bec8d386dba945582b4716a359846505
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-10-30 23:51:15 +0530

    Make it public

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,3 +1,4 @@
+import { AssetPriceResolver } from './assetPrice';
 import { BalanceInfoResolver } from './balanceInfo';
 import { MarketMetadataResolver } from './marketMetadata';
 import { MarketStatsResolver } from './marketStats';
@@ -6,6 +7,7 @@ import { PriceHistoryResolver } from './priceHistory';
 import { StatsResolver } from './stats';
 
 export {
+  AssetPriceResolver,
   BalanceInfoResolver,
   MarketMetadataResolver,
   MarketStatsResolver,
