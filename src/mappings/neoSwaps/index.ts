27328870f8f1f42891945ede52d28b82086cafe2
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-11-11 17:33:58 +0530

    Setup prices post buy and sell execution

--- src/mappings/neoSwaps/index.ts
+++ src/mappings/neoSwaps/index.ts
@@ -10,35 +10,88 @@ import {
   Market,
   MarketEvent,
   NeoPool,
-  PoolStatus,
 } from '../../model';
 import { Ctx, EventItem } from '../../processor';
-import { extrinsicFromEvent, isBaseAsset } from '../helper';
+import { calculateSpotPrice, extrinsicFromEvent, isBaseAsset } from '../helper';
 import { getBuyExecutedEvent, getPoolDeployedEvent, getSellExecutedEvent } from './types';
 
 export const buyExecuted = async (
   ctx: Ctx,
   block: SubstrateBlock,
   item: EventItem
-): Promise<HistoricalSwap | undefined> => {
-  let { who, marketId, asset, amountIn, amountOut } = getBuyExecutedEvent(ctx, item);
+): Promise<
+  { historicalAssets: HistoricalAsset[]; historicalSwap: HistoricalSwap; historicalPool: HistoricalPool } | undefined
+> => {
+  const { who, marketId, assetExecuted, amountIn, amountOut } = getBuyExecutedEvent(ctx, item);
 
-  const market = await ctx.store.get(Market, { where: { marketId: +marketId.toString() } });
-  if (!market) return;
+  const market = await ctx.store.get(Market, {
+    where: { marketId: +marketId.toString() },
+    relations: { neoPool: { account: { balances: true } } },
+  });
+  if (!market || !market.neoPool) return;
+
+  market.neoPool.volume = market.neoPool.volume + amountIn;
+  console.log(`[${item.event.name}] Saving pool: ${JSON.stringify(market.neoPool, null, 2)}`);
+  await ctx.store.save<NeoPool>(market.neoPool);
+
+  const historicalPool = new HistoricalPool({
+    blockNumber: block.height,
+    dVolume: amountIn,
+    event: item.event.name.split('.')[1],
+    id: item.event.id + '-' + market.marketId,
+    poolId: market.marketId,
+    status: null,
+    timestamp: new Date(block.timestamp),
+    volume: market.neoPool.volume,
+  });
+
+  const historicalAssets: HistoricalAsset[] = [];
+  await Promise.all(
+    market.neoPool.account.balances.map(async (ab) => {
+      if (isBaseAsset(ab.assetId)) return;
+      const asset = await ctx.store.get(Asset, {
+        where: { assetId: ab.assetId },
+      });
+      if (!asset) return;
+
+      const oldPrice = asset.price;
+      const oldAmountInPool = asset.amountInPool;
+      asset.amountInPool = ab.balance;
+      asset.price = calculateSpotPrice(ab.balance, market.neoPool!.liquidityParameter);
+      console.log(`[${item.event.name}] Saving asset: ${JSON.stringify(asset, null, 2)}`);
+      await ctx.store.save<Asset>(asset);
+
+      const ha = new HistoricalAsset({
+        accountId: asset.assetId === assetExecuted ? who : null,
+        assetId: asset.assetId,
+        baseAssetTraded: asset.assetId === assetExecuted ? amountIn : null,
+        blockNumber: block.height,
+        dAmountInPool: asset.amountInPool - oldAmountInPool,
+        dPrice: asset.price - oldPrice,
+        event: item.event.name.split('.')[1],
+        id: item.event.id + '-' + asset.id.substring(asset.id.lastIndexOf('-') + 1),
+        newAmountInPool: asset.amountInPool,
+        newPrice: asset.price,
+        timestamp: new Date(block.timestamp),
+      });
+      historicalAssets.push(ha);
+    })
+  );
 
   const historicalSwap = new HistoricalSwap({
     accountId: who,
     assetAmountIn: amountIn,
     assetAmountOut: amountOut,
     assetIn: market.baseAsset,
-    assetOut: asset,
+    assetOut: assetExecuted,
     blockNumber: block.height,
     event: item.event.name.split('.')[1],
     extrinsic: extrinsicFromEvent(item.event),
     id: item.event.id,
     timestamp: new Date(block.timestamp),
   });
-  return historicalSwap;
+
+  return { historicalAssets, historicalSwap, historicalPool };
 };
 
 export const poolDeployed = async (
@@ -76,6 +129,7 @@ export const poolDeployed = async (
     marketId: +marketId.toString(),
     poolId: +marketId.toString(),
     swapFee,
+    volume: BigInt(0),
   });
   console.log(`[${item.event.name}] Saving neo pool: ${JSON.stringify(neoPool, null, 2)}`);
   await ctx.store.save<NeoPool>(neoPool);
@@ -111,7 +165,7 @@ export const poolDeployed = async (
         amountInPool: ab.balance,
         id: item.event.id + '-' + neoPool.marketId + i,
         market: market,
-        price: 0,
+        price: calculateSpotPrice(ab.balance, neoPool.liquidityParameter),
       });
       console.log(`[${item.event.name}] Saving asset: ${JSON.stringify(asset, null, 2)}`);
       await ctx.store.save<Asset>(asset);
@@ -135,13 +189,13 @@ export const poolDeployed = async (
 
   const historicalPool = new HistoricalPool({
     blockNumber: block.height,
-    dVolume: BigInt(0),
+    dVolume: neoPool.volume,
     event: item.event.name.split('.')[1],
-    id: item.event.id + '-' + neoPool.poolId,
-    poolId: neoPool.poolId,
+    id: item.event.id + '-' + market.marketId,
+    poolId: market.marketId,
     status: null,
     timestamp: new Date(block.timestamp),
-    volume: BigInt(0),
+    volume: neoPool.volume,
   });
 
   return { historicalAssets, historicalPool };
@@ -151,17 +205,70 @@ export const sellExecuted = async (
   ctx: Ctx,
   block: SubstrateBlock,
   item: EventItem
-): Promise<HistoricalSwap | undefined> => {
-  let { who, marketId, asset, amountIn, amountOut } = getSellExecutedEvent(ctx, item);
+): Promise<
+  { historicalAssets: HistoricalAsset[]; historicalSwap: HistoricalSwap; historicalPool: HistoricalPool } | undefined
+> => {
+  const { who, marketId, assetExecuted, amountIn, amountOut } = getSellExecutedEvent(ctx, item);
 
-  const market = await ctx.store.get(Market, { where: { marketId: +marketId.toString() } });
-  if (!market) return;
+  const market = await ctx.store.get(Market, {
+    where: { marketId: +marketId.toString() },
+    relations: { neoPool: { account: { balances: true } } },
+  });
+  if (!market || !market.neoPool) return;
+
+  market.neoPool.volume = market.neoPool.volume + amountOut;
+  console.log(`[${item.event.name}] Saving pool: ${JSON.stringify(market.neoPool, null, 2)}`);
+  await ctx.store.save<NeoPool>(market.neoPool);
+
+  const historicalPool = new HistoricalPool({
+    blockNumber: block.height,
+    dVolume: amountOut,
+    event: item.event.name.split('.')[1],
+    id: item.event.id + '-' + market.marketId,
+    poolId: market.marketId,
+    status: null,
+    timestamp: new Date(block.timestamp),
+    volume: market.neoPool.volume,
+  });
+
+  const historicalAssets: HistoricalAsset[] = [];
+  await Promise.all(
+    market.neoPool.account.balances.map(async (ab) => {
+      if (isBaseAsset(ab.assetId)) return;
+      const asset = await ctx.store.get(Asset, {
+        where: { assetId: ab.assetId },
+      });
+      if (!asset) return;
+
+      const oldPrice = asset.price;
+      const oldAmountInPool = asset.amountInPool;
+      asset.amountInPool = ab.balance;
+      asset.price = calculateSpotPrice(ab.balance, market.neoPool!.liquidityParameter);
+      console.log(`[${item.event.name}] Saving asset: ${JSON.stringify(asset, null, 2)}`);
+      await ctx.store.save<Asset>(asset);
+
+      const ha = new HistoricalAsset({
+        accountId: asset.assetId === assetExecuted ? who : null,
+        assetId: asset.assetId,
+        baseAssetTraded: asset.assetId === assetExecuted ? amountIn : null,
+        blockNumber: block.height,
+        dAmountInPool: asset.amountInPool - oldAmountInPool,
+        dPrice: asset.price - oldPrice,
+        event: item.event.name.split('.')[1],
+        id: item.event.id + '-' + asset.id.substring(asset.id.lastIndexOf('-') + 1),
+        newAmountInPool: asset.amountInPool,
+        newPrice: asset.price,
+        timestamp: new Date(block.timestamp),
+      });
+      historicalAssets.push(ha);
+    })
+  );
 
   const historicalSwap = new HistoricalSwap({
     accountId: who,
     assetAmountIn: amountIn,
     assetAmountOut: amountOut,
-    assetIn: asset,
+    assetIn: assetExecuted,
     assetOut: market.baseAsset,
     blockNumber: block.height,
     event: item.event.name.split('.')[1],
@@ -169,5 +276,6 @@ export const sellExecuted = async (
     id: item.event.id,
     timestamp: new Date(block.timestamp),
   });
-  return historicalSwap;
+
+  return { historicalAssets, historicalSwap, historicalPool };
 };
