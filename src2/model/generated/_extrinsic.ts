b16235882be1b8cdae67fa65a4fc57c8e59535d6
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2023-12-21 12:21:19 +0530

    Make schema changes for `Extrinsic`

--- src2/model/generated/_extrinsic.ts
+++ src2/model/generated/_extrinsic.ts
@@ -6,11 +6,13 @@ import * as marshal from "./marshal"
  */
 export class Extrinsic {
     private _hash!: string
+    private _name!: string | undefined | null
 
     constructor(props?: Partial<Omit<Extrinsic, 'toJSON'>>, json?: any) {
         Object.assign(this, props)
         if (json != null) {
             this._hash = marshal.string.fromJSON(json.hash)
+            this._name = json.name == null ? undefined : marshal.string.fromJSON(json.name)
         }
     }
 
@@ -26,9 +28,21 @@ export class Extrinsic {
         this._hash = value
     }
 
+    /**
+     * Name of the extrinsic
+     */
+    get name(): string | undefined | null {
+        return this._name
+    }
+
+    set name(value: string | undefined | null) {
+        this._name = value
+    }
+
     toJSON(): object {
         return {
             hash: this.hash,
+            name: this.name,
         }
     }
 }
