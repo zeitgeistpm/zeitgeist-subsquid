f5edec364df5bfe59cc152002e5e976cc117e836
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-01-14 15:18:26 +0530

    Segregate code for resolvers

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,3 +1,5 @@
+import { registerEnumType } from 'type-graphql';
+import { BalanceInfoEvent, BaseAsset, OrderBy, TargetAsset, Unit, _Asset } from '../../consts';
 import { AssetPriceResolver } from './assetPrice';
 import { BalanceInfoResolver } from './balanceInfo';
 import { MarketMetadataResolver } from './marketMetadata';
@@ -6,6 +8,34 @@ import { MarketStatsWithOrderResolver } from './marketStatsWithOrder';
 import { PriceHistoryResolver } from './priceHistory';
 import { StatsResolver } from './stats';
 import { VolumeHistoryResolver } from './volumeHistory';
+
+registerEnumType(_Asset, {
+  name: 'AssetKind',
+  description: 'Kind of asset',
+});
+
+registerEnumType(BalanceInfoEvent, {
+  name: 'BalanceInfoEvent',
+  description: 'Filtering balance based on event',
+});
+
+registerEnumType(BaseAsset, {
+  name: 'BaseAsset',
+});
+
+registerEnumType(OrderBy, {
+  name: 'OrderBy',
+  description: 'Ordering stats',
+});
+
+registerEnumType(TargetAsset, {
+  name: 'TargetAsset',
+});
+
+registerEnumType(Unit, {
+  name: 'Unit',
+  description: 'Unit for the interval',
+});
 
 export {
   AssetPriceResolver,
