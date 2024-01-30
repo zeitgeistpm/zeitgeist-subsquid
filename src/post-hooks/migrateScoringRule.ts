4deffa0e64f2738827eb0b1c6581f6bb3093a895
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-01-30 23:59:41 +0530

    Reformat

--- src/post-hooks/migrateScoringRule.ts
+++ src/post-hooks/migrateScoringRule.ts
@@ -4,11 +4,15 @@ import { Market, ScoringRule } from '../model';
 export const migrateScoringRule = async (store: Store) => {
   const eventName = 'PostHooks.MigrateScoringRule';
 
-  const markets = await store.find(Market, { where: { scoringRule: ScoringRule.CPMM } });
-  for (let i = 0; i < markets.length; i++) {
-    const market = markets[i];
-    market.scoringRule = ScoringRule.Lmsr;
-    console.log(`[${eventName}] Saving market: ${JSON.stringify(market, null, 2)}`);
-    await store.save<Market>(market);
-  }
+  const marketIds = [0, 9, 534];
+
+  await Promise.all(
+    marketIds.map(async (marketId) => {
+      const market = await store.get(Market, { where: { marketId } });
+      if (!market) return;
+      market.scoringRule = ScoringRule.Lmsr;
+      console.log(`[${eventName}] Saving market: ${JSON.stringify(market, null, 2)}`);
+      await store.save<Market>(market);
+    })
+  );
 };
