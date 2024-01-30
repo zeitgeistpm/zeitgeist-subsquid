2ae6a7665cf31644db1a6c4482c24d5c7ca9d222
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-01-30 23:02:22 +0530

    Migrate from array mapping to for loop

--- src/post-hooks/migrateScoringRule.ts
+++ src/post-hooks/migrateScoringRule.ts
@@ -5,13 +5,10 @@ export const migrateScoringRule = async (store: Store) => {
   const eventName = 'PostHooks.MigrateScoringRule';
 
   const markets = await store.find(Market, { where: { scoringRule: ScoringRule.CPMM } });
-  if (!markets) return;
-
-  await Promise.all(
-    markets.map(async (market) => {
-      market.scoringRule = ScoringRule.Lmsr;
-      console.log(`[${eventName}] Saving market: ${JSON.stringify(market, null, 2)}`);
-      await store.save<Market>(market);
-    })
-  );
+  for (let i = 0; i < markets.length; i++) {
+    const market = markets[i];
+    market.scoringRule = ScoringRule.Lmsr;
+    console.log(`[${eventName}] Saving market: ${JSON.stringify(market, null, 2)}`);
+    await store.save<Market>(market);
+  }
 };
