9b2627a89ca120dafa30dfee8c19aeacc262a770
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2024-02-06 12:39:35 +0530

    Debug initialisation of OutcomeReport (#514)
    * Set `undefined` instead of `null` for fields on outcome-report
    
    * Reset outcome-report values on market-reported
    
    * Adapt similar changes on authorityReported

--- src/mappings/prediction-markets/index.ts
+++ src/mappings/prediction-markets/index.ts
@@ -483,8 +483,8 @@ export const marketDisputed = async (store: Store, event: Event) => {
       by: accountId,
       outcome: outcome
         ? new OutcomeReport({
-            categorical: outcome.__kind == 'Categorical' ? outcome.value : null,
-            scalar: outcome.__kind == 'Scalar' ? outcome.value : null,
+            categorical: outcome.__kind === 'Categorical' ? outcome.value : undefined,
+            scalar: outcome.__kind === 'Scalar' ? outcome.value : undefined,
           })
         : null,
     });
@@ -642,8 +642,8 @@ export const marketReported = async (store: Store, event: Event) => {
   if (!market) return;
 
   const outcomeReport = new OutcomeReport({
-    categorical: outcome.__kind == 'Categorical' ? outcome.value : null,
-    scalar: outcome.__kind == 'Scalar' ? outcome.value : null,
+    categorical: outcome.__kind === 'Categorical' ? outcome.value : undefined,
+    scalar: outcome.__kind === 'Scalar' ? outcome.value : undefined,
   });
 
   const marketReport = new MarketReport({
