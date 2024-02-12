bbfb81cc2dc864409a60c5d267b52f75003e169c
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-02-12 12:28:18 +0530

    Cleanup handler `newAccount`

--- src/mappings/system/index.ts
+++ src/mappings/system/index.ts
@@ -1,17 +1,16 @@
-import { Store } from '@subsquid/typeorm-store';
 import * as ss58 from '@subsquid/ss58';
-import { Account, AccountBalance, HistoricalAccountBalance } from '../../model';
+import { HistoricalAccountBalance } from '../../model';
 import { Pallet, _Asset } from '../../consts';
 import { extrinsicFromEvent, getFees } from '../../helper';
 import { Event } from '../../processor';
-import { decodeExtrinsicFailedEvent, decodeExtrinsicSuccessEvent, decodeNewAccountEvent } from './decode';
+import * as decode from './decode';
 
 export const extrinsicFailed = async (event: Event): Promise<HistoricalAccountBalance | undefined> => {
   if (!event.extrinsic || !event.extrinsic.signature || !event.extrinsic.signature.address) return;
   const accountId = ss58.encode({ prefix: 73, bytes: (event.extrinsic.signature.address as any).value });
 
-  const { dispatchInfo } = decodeExtrinsicFailedEvent(event);
-  if (dispatchInfo.paysFee.__kind == 'No') return;
+  const dispatchInfo = decode.extrinsicFailed(event);
+  if (dispatchInfo.paysFee.__kind === 'No') return;
 
   const hab = new HistoricalAccountBalance({
     accountId,
@@ -36,7 +35,7 @@ export const extrinsicSuccess = async (event: Event): Promise<HistoricalAccountB
     return;
   const accountId = ss58.encode({ prefix: 73, bytes: (event.extrinsic.signature.address as any).value });
 
-  const { dispatchInfo } = decodeExtrinsicSuccessEvent(event);
+  const dispatchInfo = decode.extrinsicSuccess(event);
   if (dispatchInfo.paysFee.__kind === 'No') return;
 
   const hab = new HistoricalAccountBalance({
@@ -50,39 +49,4 @@ export const extrinsicSuccess = async (event: Event): Promise<HistoricalAccountB
     timestamp: new Date(event.block.timestamp!),
   });
   return hab;
-};
-
-export const newAccount = async (store: Store, event: Event) => {
-  const { accountId } = decodeNewAccountEvent(event);
-
-  const account = await store.get(Account, { where: { accountId } });
-  if (account) return;
-
-  const newAccount = new Account({
-    accountId,
-    id: accountId,
-  });
-  console.log(`[${event.name}] Saving account: ${JSON.stringify(newAccount, null, 2)}`);
-  await store.save<Account>(newAccount);
-
-  const ab = new AccountBalance({
-    account: newAccount,
-    assetId: _Asset.Ztg,
-    balance: BigInt(0),
-    id: event.id + '-' + accountId.slice(-5),
-  });
-  console.log(`[${event.name}] Saving account balance: ${JSON.stringify(ab, null, 2)}`);
-  await store.save<AccountBalance>(ab);
-
-  const hab = new HistoricalAccountBalance({
-    accountId,
-    assetId: _Asset.Ztg,
-    blockNumber: event.block.height,
-    dBalance: BigInt(0),
-    event: event.name.split('.')[1],
-    id: event.id + '-' + accountId.slice(-5),
-    timestamp: new Date(event.block.timestamp!),
-  });
-  console.log(`[${event.name}] Saving historical account balance: ${JSON.stringify(hab, null, 2)}`);
-  await store.save<HistoricalAccountBalance>(hab);
 };
