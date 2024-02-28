6a3135c948e334b8073cb319fcf50b85018f1000
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-02-28 17:55:16 +0530

    Strengthen types for order from order-placed-event

--- src/mappings/orderbook/decode.ts
+++ src/mappings/orderbook/decode.ts
@@ -1,11 +1,13 @@
 import * as ss58 from '@subsquid/ss58';
+import { Order as Order_v51 } from '../../types/v51';
+import { Order as Order_v53 } from '../../types/v53';
 import { events } from '../../types';
 import { _Asset } from '../../consts';
 import { formatAssetId } from '../../helper';
 import { Event } from '../../processor';
 
 export const orderPlaced = (event: Event): OrderPlacedEvent => {
-  let decoded: { orderId: bigint; order: any };
+  let decoded: { orderId: bigint; order: Order_v51 | Order_v53 };
   if (events.orderbook.orderPlaced.v50.is(event)) {
     decoded = events.orderbook.orderPlaced.v50.decode(event);
   } else if (events.orderbook.orderPlaced.v51.is(event)) {
@@ -15,14 +17,27 @@ export const orderPlaced = (event: Event): OrderPlacedEvent => {
   } else {
     decoded = event.args;
   }
+
+  if ('baseAsset' in decoded.order) {
+    return {
+      orderId: decoded.orderId.toString(),
+      marketId: Number(decoded.order.marketId),
+      maker: ss58.encode({ prefix: 73, bytes: decoded.order.maker }),
+      makerAsset: formatAssetId(decoded.order.baseAsset),
+      makerAmount: BigInt(decoded.order.baseAssetAmount),
+      takerAsset: formatAssetId(decoded.order.outcomeAsset),
+      takerAmount: BigInt(decoded.order.outcomeAssetAmount),
+    };
+  }
+
   return {
     orderId: decoded.orderId.toString(),
     marketId: Number(decoded.order.marketId),
     maker: ss58.encode({ prefix: 73, bytes: decoded.order.maker }),
-    makerAsset: formatAssetId(decoded.order.makerAsset ?? decoded.order.baseAsset),
-    makerAmount: BigInt(decoded.order.makerAmount ?? decoded.order.baseAssetAmount),
-    takerAsset: formatAssetId(decoded.order.takerAsset ?? decoded.order.outcomeAsset),
-    takerAmount: BigInt(decoded.order.takerAmount ?? decoded.order.baseAssetAmount),
+    makerAsset: formatAssetId(decoded.order.makerAsset),
+    makerAmount: BigInt(decoded.order.makerAmount),
+    takerAsset: formatAssetId(decoded.order.takerAsset),
+    takerAmount: BigInt(decoded.order.takerAmount),
   };
 };
 
