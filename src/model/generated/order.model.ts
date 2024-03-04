494a83f4276ca16991b6780d415ad00a9b454af2
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-03-04 19:28:26 +0530

    Regenerate code based on schema changes

---
+++ src/model/generated/order.model.ts
@@ -0,0 +1,35 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, Index as Index_} from "typeorm"
+import * as marshal from "./marshal"
+import {OrderRecord} from "./_orderRecord"
+
+/**
+ * A type that records the order history of a market.
+ */
+@Entity_()
+export class Order {
+    constructor(props?: Partial<Order>) {
+        Object.assign(this, props)
+    }
+
+    @Column_("timestamp with time zone", {nullable: false})
+    createdAt!: Date
+
+    @PrimaryColumn_()
+    id!: string
+
+    @Column_("text", {nullable: false})
+    makerAccountId!: string
+
+    @Column_("jsonb", {transformer: {to: obj => obj.toJSON(), from: obj => obj == null ? undefined : new OrderRecord(undefined, obj)}, nullable: false})
+    maker!: OrderRecord
+
+    @Column_("jsonb", {transformer: {to: obj => obj.toJSON(), from: obj => obj == null ? undefined : new OrderRecord(undefined, obj)}, nullable: false})
+    taker!: OrderRecord
+
+    @Index_()
+    @Column_("int4", {nullable: false})
+    marketId!: number
+
+    @Column_("timestamp with time zone", {nullable: false})
+    updatedAt!: Date
+}
