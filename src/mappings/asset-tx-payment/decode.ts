98450c8a471c1aa0fab9e85019f7d473e96dc129
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-05-09 09:17:26 +0530

    Decode assetId on AssetTxFeePaidEvent

--- src/mappings/asset-tx-payment/decode.ts
+++ src/mappings/asset-tx-payment/decode.ts
@@ -1,31 +1,30 @@
 import * as ss58 from '@subsquid/ss58';
+import { Asset } from '../../types/v54';
 import { events } from '../../types';
+import { _Asset } from '../../consts';
 import { Event } from '../../processor';
 
 export const decodeAssetTxFeePaidEvent = (event: Event): AssetTxFeePaidEvent => {
-  let decoded: { who: string; actualFee: bigint; assetId?: any; assetIdValue?: number };
+  let decoded: { who: string; actualFee: bigint; assetId?: any };
   if (events.assetTxPayment.assetTxFeePaid.v47.is(event)) {
     decoded = events.assetTxPayment.assetTxFeePaid.v47.decode(event);
-    decoded.assetIdValue = decoded.assetId.value;
   } else if (events.assetTxPayment.assetTxFeePaid.v48.is(event)) {
     decoded = events.assetTxPayment.assetTxFeePaid.v48.decode(event);
-    decoded.assetIdValue = decoded.assetId;
+    decoded.assetId = { __kind: _Asset.ForeignAsset, value: decoded.assetId };
   } else if (events.assetTxPayment.assetTxFeePaid.v54.is(event)) {
     decoded = events.assetTxPayment.assetTxFeePaid.v54.decode(event);
-    decoded.assetIdValue = decoded.assetId.value;
   } else {
     decoded = event.args;
-    decoded.assetIdValue = decoded.assetId.value;
   }
   return {
     accountId: ss58.encode({ prefix: 73, bytes: decoded.who }),
+    assetId: decoded.assetId,
     actualFee: BigInt(decoded.actualFee),
-    assetIdValue: Number(decoded.assetIdValue),
   };
 };
 
 interface AssetTxFeePaidEvent {
   accountId: string;
+  assetId?: Asset;
   actualFee: bigint;
-  assetIdValue: number;
 }
