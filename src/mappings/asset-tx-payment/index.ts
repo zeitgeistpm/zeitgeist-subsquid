af35c9a59b9282dad5ecc2d9041474369549c4fd
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-05-09 09:18:27 +0530

    Remap newly decoded params

--- src/mappings/asset-tx-payment/index.ts
+++ src/mappings/asset-tx-payment/index.ts
@@ -7,29 +7,30 @@ import { decodeMetadataStorage } from '../asset-registry/decode';
 import { decodeAssetTxFeePaidEvent } from './decode';
 
 export const assetTxFeePaid = async (event: Event): Promise<HistoricalAccountBalance[] | undefined> => {
-  const { accountId, actualFee, assetIdValue } = decodeAssetTxFeePaidEvent(event);
-  const assetId: Asset_ForeignAsset = { __kind: _Asset.ForeignAsset, value: assetIdValue };
-  const habs: HistoricalAccountBalance[] = [];
+  const { accountId, assetId, actualFee } = decodeAssetTxFeePaidEvent(event);
+  if (!assetId) return;
 
-  const onChainAsset = await decodeMetadataStorage(event.block, assetId);
-  if (!onChainAsset || !onChainAsset.additional.xcm.feeFactor) return;
-  const amount = (Number(actualFee) * Number(onChainAsset.additional.xcm.feeFactor)) / 10 ** 10;
+  let amount = Number(actualFee);
+  if (assetId.__kind === _Asset.ForeignAsset) {
+    const onChainAsset = await decodeMetadataStorage(event.block, assetId);
+    if (!onChainAsset || !onChainAsset.additional.xcm.feeFactor) return;
+    amount = Math.round((Number(actualFee) * Number(onChainAsset.additional.xcm.feeFactor)) / 10 ** 10);
+  }
 
   const userHab = new HistoricalAccountBalance({
     accountId,
     assetId: formatAssetId(assetId),
     blockNumber: event.block.height,
-    dBalance: -BigInt(Math.round(amount)),
+    dBalance: -BigInt(amount),
     event: event.name.split('.')[1],
     extrinsic: extrinsicFromEvent(event),
     id: event.id + '-' + accountId.slice(-5),
     timestamp: new Date(event.block.timestamp!),
   });
-  habs.push(userHab);
 
   // Skip condition. Ref: https://github.com/zeitgeistpm/zeitgeist/issues/1091
   if (isBatteryStation() && event.block.specVersion < 49) {
-    if (assetIdValue == 0 || assetIdValue == 2) return habs;
+    if ((assetId as Asset_ForeignAsset).value === 0 || (assetId as Asset_ForeignAsset).value === 2) return [userHab];
   }
 
   // Record TokensBalanceSetEvent for txn fee deposits in treasury account
@@ -37,13 +38,12 @@ export const assetTxFeePaid = async (event: Event): Promise<HistoricalAccountBal
     accountId: TREASURY_ACCOUNT,
     assetId: formatAssetId(assetId),
     blockNumber: event.block.height,
-    dBalance: BigInt(Math.round(amount)),
+    dBalance: BigInt(amount),
     event: event.name.split('.')[1],
     extrinsic: extrinsicFromEvent(event),
     id: event.id + '-' + TREASURY_ACCOUNT.slice(-5),
     timestamp: new Date(event.block.timestamp!),
   });
-  habs.push(treasuryHab);
 
-  return habs;
+  return [userHab, treasuryHab];
 };
