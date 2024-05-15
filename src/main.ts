6285bb0a4c59e82127065d54bae8f79f46f16481
Nikhil Saboo <36529278+saboonikhil@users.noreply.github.com>
2024-05-15 14:26:02 +0530

    Migrate from lmsr to ammCdaHybrid (#557)
    * Create migration method from Lmsr to AmmCdaHybrid
    
    * Wire post-hooks with processor
    
    * Set to block-number which is traversable
    
    * Enable migration for mainnet

--- src/main.ts
+++ src/main.ts
@@ -127,7 +127,9 @@ const handleBsrPostHooks = async (store: Store, blockHeight: number) => {
     await saveAccounts(store);
     await postHooks.destroyMarkets(store);
   } else if (blockHeight === 4772816) {
-    await postHooks.migrateScoringRule(store);
+    await postHooks.migrateToLmsr(store);
+  } else if (blockHeight === 4969221) {
+    await postHooks.migrateToAmmCdaHybrid(store);
   }
 };
 
@@ -136,7 +138,9 @@ const handleMainPostHooks = async (store: Store, blockHeight: number) => {
     const historicalAccountBalances = await postHooks.initBalance();
     await storeBalanceChanges(historicalAccountBalances);
   } else if (blockHeight === 4793019) {
-    await postHooks.migrateScoringRule(store);
+    await postHooks.migrateToLmsr(store);
+  } else if (blockHeight === 5521822) {
+    await postHooks.migrateToAmmCdaHybrid(store);
   }
 };
 
--- src/post-hooks/scoringRule.ts
+++ src/post-hooks/scoringRule.ts
@@ -1,7 +1,22 @@
 import { Store } from '@subsquid/typeorm-store';
 import { Market, MarketStatus, ScoringRule } from '../model';
 
-export const migrateScoringRule = async (store: Store) => {
+export const migrateToAmmCdaHybrid = async (store: Store) => {
+  const eventName = 'PostHooks.MigrateScoringRule';
+  const updatedMarkets: Market[] = [];
+
+  const markets = await store.findBy(Market, { scoringRule: ScoringRule.Lmsr });
+  await Promise.all(
+    markets.map(async (market) => {
+      market.scoringRule = ScoringRule.AmmCdaHybrid;
+      updatedMarkets.push(market);
+    })
+  );
+  console.log(`[${eventName}] Saving markets: ${JSON.stringify(updatedMarkets, null, 2)}`);
+  await store.save<Market>(updatedMarkets);
+};
+
+export const migrateToLmsr = async (store: Store) => {
   const eventName = 'PostHooks.MigrateScoringRule';
   const updatedMarkets: Market[] = [];
 
