7f154ff5712fe14013d094eb8de5f4cc28b91123
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-05-21 13:57:14 +0530

    Generate types for `Court.JurorRevealedVote`

--- src/types/court/events.ts
+++ src/types/court/events.ts
@@ -1,6 +1,7 @@
 import {sts, Block, Bytes, Option, Result, EventType, RuntimeCtx} from '../support'
 import * as v49 from '../v49'
 import * as v51 from '../v51'
+import * as v53 from '../v53'
 
 export const jurorVoted =  {
     name: 'Court.JurorVoted',
@@ -13,6 +14,36 @@ export const jurorVoted =  {
             courtId: sts.bigint(),
             juror: v49.AccountId32,
             commitment: v49.H256,
+        })
+    ),
+}
+
+export const jurorRevealedVote =  {
+    name: 'Court.JurorRevealedVote',
+    /**
+     * A juror has revealed their vote.
+     */
+    v49: new EventType(
+        'Court.JurorRevealedVote',
+        sts.struct({
+            juror: v49.AccountId32,
+            courtId: sts.bigint(),
+            voteItem: v49.VoteItem,
+            salt: v49.H256,
+        })
+    ),
+    /**
+     * A juror has revealed their vote.
+     */
+    v53: new EventType(
+        'Court.JurorRevealedVote',
+        sts.struct({
+            juror: v53.AccountId32,
+            courtId: sts.bigint(),
+            voteItem: v53.VoteItem,
+            salt: v53.H256,
+            slashableAmount: sts.bigint(),
+            drawWeight: sts.number(),
         })
     ),
 }
